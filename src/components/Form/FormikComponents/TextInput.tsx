24be00a2b0d9243c436d9d08ad174118b32ac4a9
Retr0327 <philcoke35@gmail.com>
2022-10-29 15:52:48 +0800

    Add TextInput

---
+++ src/components/Form/FormikComponents/TextInput.tsx
@@ -0,0 +1,23 @@
+import { TextInputProps } from 'types';
+import useCustomFormik from '@hooks/Formik';
+import { TextInput as MantineTextInput } from '@mantine/core';
+
+function TextInput(props: TextInputProps) {
+  const { label, name, ...rest } = props;
+  const [formik, hasError] = useCustomFormik(name);
+  const inputValue = formik.values[name] as TextInputProps['value'];
+
+  return (
+    <MantineTextInput
+      label={label}
+      name={name}
+      value={inputValue ?? undefined}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      error={hasError}
+      {...rest}
+    />
+  );
+}
+
+export default TextInput;
