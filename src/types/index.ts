51347b1ce4d13a8891e8efc744bb8b966a0d87e4
Retr0327 <philcoke35@gmail.com>
2022-10-29 15:47:58 +0800

    Add BlacklabResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+// ---------- API ----------
+
 export type HTTPMethods =
   | 'GET'
   | 'POST'
@@ -22,8 +24,95 @@ export interface Response<ResData = {}> {
   msg?: string;
 }
 
+// ----- API response -----
+
+export interface BlacklabResponse {
+  summary: Summary;
+  hits: Hit[];
+  docInfos: DocInfos;
+}
+
+export interface DocInfos {
+  [key: string]: {
+    fromInputFile: string[];
+    year: string[];
+    author: string[];
+    media: string[];
+    title: string[];
+    doc_id: string[];
+    board: string[];
+    lengthInTokens: number;
+    mayView: boolean;
+  };
+}
+
+export interface Hit {
+  docPid: string;
+  start: number;
+  end: number;
+  left: HitData;
+  match: HitData;
+  right: HitData;
+}
+
+export interface HitData {
+  punct: string[];
+  pos: string[];
+  word: string[];
+}
+
+export interface Summary {
+  searchParam: SearchParam;
+  searchTime: number;
+  countTime: number;
+  windowFirstResult: number;
+  requestedWindowSize: number;
+  actualWindowSize: number;
+  windowHasPrevious: boolean;
+  windowHasNext: boolean;
+  stillCounting: boolean;
+  numberOfHits: number;
+  numberOfHitsRetrieved: number;
+  stoppedCountingHits: boolean;
+  stoppedRetrievingHits: boolean;
+  numberOfDocs: number;
+  numberOfDocsRetrieved: number;
+  docFields: DocFields;
+  metadataFieldDisplayNames: MetadataFieldDisplayNames;
+}
+
+export interface DocFields {
+  pidField: string;
+  titleField: string;
+  authorField: string;
+  dateField: string;
+}
+
+export interface MetadataFieldDisplayNames {
+  author: string;
+  board: string;
+  doc_id: string;
+  fromInputFile: string;
+  media: string;
+  title: string;
+  year: string;
+}
+
+export interface SearchParam {
+  filter: string;
+  first: string;
+  indexname: string;
+  number: string;
+  patt: string;
+  wordsaroundhit: string;
+}
+
+// -------- mantine --------
+
 // -------- helpers --------
 
 export type PickAsOrNull<T, K extends keyof T> = Omit<T, K> & {
   [P in K]: T[P] | null;
 };
+
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
