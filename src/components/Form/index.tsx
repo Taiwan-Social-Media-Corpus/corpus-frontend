d64e3152d3a55f6117b95a7db6b3ca272c644122
Retr0327 <philcoke35@gmail.com>
2022-10-29 16:17:40 +0800

    Add switch control

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -10,9 +10,33 @@ function FormikController(props: ControllerProps) {
       const Select = dynamic(() => import('./FormikComponents/Select'));
       return <Select {...props} />;
     }
+    case 'switch': {
+      const Switch = dynamic(() => import('./FormikComponents/Switch'));
+      return <Switch {...props} />;
+    }
     case 'text-input': {
       const TextInput = dynamic(() => import('./FormikComponents/TextInput'));
       return <TextInput {...props} />;
+    }
+    case 'radio-group': {
+      const RadioGroup = dynamic(() => import('./FormikComponents/RadioGroup'));
+      return <RadioGroup {...props} />;
+    }
+    case 'multi-select': {
+      const MultiSelect = dynamic(() => import('./FormikComponents/MultiSelect'));
+      return <MultiSelect {...props} />;
+    }
+    case 'number-input': {
+      const NumberInput = dynamic(() => import('./FormikComponents/NumberInput'));
+      return <NumberInput {...props} />;
+    }
+    case 'checkbox-group': {
+      const CheckboxGroup = dynamic(() => import('./FormikComponents/CheckboxGroup'));
+      return <CheckboxGroup {...props} />;
+    }
+    case 'segmented-control': {
+      const SegmentedControl = dynamic(() => import('./FormikComponents/SegmentedControl'));
+      return <SegmentedControl {...props} />;
     }
     default:
       return null;
