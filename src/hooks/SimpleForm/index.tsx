c169b5f4dedc00ba3ebc13432f767fa7fd60c5e3
Retr0327 <philcoke35@gmail.com>
2022-10-30 00:38:08 +0800

    Add useSimpleForm

---
+++ src/hooks/SimpleForm/index.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from 'react';
+import { useId } from '@mantine/hooks';
+import FormikController from '@components/Form';
+import { SimpleFormControllerProps } from 'types';
+import { Form, Formik, FormikConfig, FormikContextType } from 'formik';
+import { Button, Grid, ButtonProps, useMantineTheme } from '@mantine/core';
+
+type Fields = {
+  [field: string]: any;
+};
+
+type SimpleFormProps<T extends Fields> = FormikConfig<T> &
+  SimpleFormControllerProps<FormikContextType<T>>;
+
+const useSimpleForm = <T extends Fields>(props: SimpleFormProps<T>) => {
+  const id = useId();
+  const theme = useMantineTheme();
+
+  const { controllers, ...formikProps } = props;
+
+  const FormikWrapper = ({
+    children,
+  }: {
+    children?: ReactNode | ((formikContext: FormikContextType<T>) => ReactNode);
+  }) => (
+    <Formik {...formikProps}>
+      {(formik) => (
+        <Form id={id}>
+          <Grid justify="center" gutter="xl">
+            {controllers.map((field, index) => {
+              const { col, after, ...controllerProps } = field;
+              return (
+                <Grid.Col key={`${field.name}-${index}`} {...col}>
+                  <FormikController {...controllerProps} />
+                  {typeof after === 'function' ? after(formik) : after}
+                </Grid.Col>
+              );
+            })}
+          </Grid>
+          {typeof children === 'function' ? children(formik) : children}
+        </Form>
+      )}
+    </Formik>
+  );
+
+  FormikWrapper.Button = (buttonProps: ButtonProps) => (
+    <Button type="submit" form={id} loaderProps={{ color: theme.colors.blue[5] }} {...buttonProps}>
+      {buttonProps.children}
+    </Button>
+  );
+
+  return FormikWrapper;
+};
+
+export default useSimpleForm;
