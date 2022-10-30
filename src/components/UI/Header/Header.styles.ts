74fbec90dba19066e9d9e2938ae573e0c23a8a99
Retr0327 <philcoke35@gmail.com>
2022-10-30 11:05:53 +0800

    Add useStyles

---
+++ src/components/UI/Header/Header.styles.ts
@@ -0,0 +1,23 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+  },
+
+  inner: {
+    height: 56,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default useStyles;
