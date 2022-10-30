2b3bfec6678cdc43364ed45c49311ee656e53e22
Retr0327 <philcoke35@gmail.com>
2022-10-30 16:05:56 +0800

    Add FormValues

--- src/components/Corpus/Form/types.ts
+++ src/components/Corpus/Form/types.ts
@@ -1,3 +1,9 @@
-import { Boards } from 'types';
+import { Boards, RequestBody } from 'types';
 
 export type CorpusFormProps = { boards: Boards };
+
+export type FormValues = Omit<RequestBody, 'page' | 'start' | 'end' | 'windowSize'> & {
+  start: number;
+  end: number;
+  windowSize: number;
+};
