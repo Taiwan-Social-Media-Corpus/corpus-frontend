21b303d7c1954227f96c03544d7b73ed5686627e
Retr0327 <philcoke35@gmail.com>
2022-10-30 15:02:27 +0800

    Add version tag

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -1,12 +1,14 @@
 import { memo } from 'react';
+import Link from 'next/link';
 import { MantineLogo } from '@mantine/ds';
-import { Header as MantineHeader, Group } from '@mantine/core';
+import { Header as MantineHeader, Group, Code } from '@mantine/core';
 import Route from '@config/routes';
 import Menu from './Menu';
 import NavItems from './NavItem';
 import useStyles from './Header.styles';
 import HeaderControls from './Controls';
 import ColorSchemeToggle from '../ColorSchemeToggle';
+import corePackageJson from '../../../../package.json';
 
 const links = [
   {
@@ -27,11 +29,17 @@ function Header() {
   const { classes } = useStyles();
 
   return (
-    <MantineHeader height={56} className={classes.header} mb={20}>
+    <MantineHeader height={56} className={classes.header}>
       <div className={classes.inner}>
         <Menu />
         <Group>
           <MantineLogo size={28} />
+          <Link
+            href="https://github.com/Taiwan-Social-Media-Corpus/corpus-backend"
+            className={classes.version}
+          >
+            <Code>v {corePackageJson.version}</Code>
+          </Link>
         </Group>
         <Group>
           <Group ml={50} spacing={5} className={classes.links}>
