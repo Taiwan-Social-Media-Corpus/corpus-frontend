3d401ab3897dc07bf3fcf8fbf187dfe538202a76
Retr0327 <philcoke35@gmail.com>
2022-10-30 10:11:15 +0800

    Add HeaderControls

---
+++ src/components/UI/Header/Controls/index.tsx
@@ -0,0 +1,24 @@
+import { Group, Tooltip } from '@mantine/core';
+import { Github, Facebook, ColorScheme } from './Icons';
+
+function HeaderControls() {
+  return (
+    <Tooltip.Group openDelay={600} closeDelay={100}>
+      <Group
+        spacing="xs"
+        pr="md"
+        sx={(theme) => ({
+          [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+          },
+        })}
+      >
+        <Facebook link="https://www.facebook.com/groups/174362969268953" />
+        <Github link="https://github.com/Taiwan-Social-Media-Corpus" tooltip="Source code" />
+        <ColorScheme />
+      </Group>
+    </Tooltip.Group>
+  );
+}
+
+export default HeaderControls;
