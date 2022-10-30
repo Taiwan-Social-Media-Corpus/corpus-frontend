ac89393d5acbed3621665cfe67d37b8215ce22bd
Retr0327 <philcoke35@gmail.com>
2022-10-30 17:08:44 +0800

    Add Header

--- src/components/UI/Layout/App/index.tsx
+++ src/components/UI/Layout/App/index.tsx
@@ -7,8 +7,8 @@ import Menu from './Menu';
 import NavItems from './NavItem';
 import useStyles from './Header.styles';
 import HeaderControls from './Controls';
-import ColorSchemeToggle from '../ColorSchemeToggle';
-import corePackageJson from '../../../../package.json';
+import ColorSchemeToggle from '../../ColorSchemeToggle';
+import corePackageJson from '../../../../../package.json';
 
 const links = [
   {
@@ -29,26 +29,29 @@ function Header() {
   const { classes } = useStyles();
 
   return (
-    <MantineHeader height={56} className={classes.header}>
-      <div className={classes.inner}>
-        <Menu />
-        <Group>
-          <MantineLogo size={28} />
+    <MantineHeader height={60} className={classes.header}>
+      <Menu />
+      <Group>
+        <div className={classes.logoWrapper}>
+          <Link href="/" className={classes.logo} aria-label="Mantine">
+            <MantineLogo size={28} />
+          </Link>
+
           <Link
             href="https://github.com/Taiwan-Social-Media-Corpus/corpus-backend"
             className={classes.version}
           >
             <Code>v {corePackageJson.version}</Code>
           </Link>
+        </div>
+      </Group>
+      <Group>
+        <Group spacing={5} className={classes.links}>
+          <NavItems links={links} />
         </Group>
-        <Group>
-          <Group ml={50} spacing={5} className={classes.links}>
-            <NavItems links={links} />
-          </Group>
-          <HeaderControls />
-          <ColorSchemeToggle />
-        </Group>
-      </div>
+        <HeaderControls />
+        <ColorSchemeToggle />
+      </Group>
     </MantineHeader>
   );
 }
