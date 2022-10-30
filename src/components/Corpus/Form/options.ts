a9166d0c633906a71a7a5dfdca809c1a9b49b2c4
Retr0327 <philcoke35@gmail.com>
2022-10-30 16:06:36 +0800

    Add mediaOptions

---
+++ src/components/Corpus/Form/options.ts
@@ -0,0 +1,7 @@
+const mediaOptions = [
+  { label: 'All', value: 'all' },
+  { label: 'PTT', value: 'ptt', image: '/ptt-logo.png' },
+  { label: 'Dcard', value: 'dcard', image: '/dcard-logo.png' },
+];
+
+export default mediaOptions;
