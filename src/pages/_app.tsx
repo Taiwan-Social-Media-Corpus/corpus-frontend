96d4ca1dbb370096f80fdd60c36408e960d2ec04
Retr0327 <philcoke35@gmail.com>
2022-10-30 16:21:39 +0800

    Fix incorrect favicon href

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 import { getCookie } from 'cookies-next';
 import Header from '@components/UI/Header';
@@ -8,13 +9,25 @@ import DarkThemeContext from '@contexts/DarkTheme';
 
 function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps, colorScheme } = props;
+  const router = useRouter();
 
   return (
     <>
       <Head>
         <title>Taiwan Social Media Corpus</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="shortcut icon"
+          href={`${router.basePath}/favicon.ico`}
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          rel="shortcut icon"
+          href={`${router.basePath}/favicon.svg`}
+          type="image/svg+xml"
+          sizes="any"
+        />
       </Head>
       <DarkThemeContext colorScheme={colorScheme}>
         <Header />
