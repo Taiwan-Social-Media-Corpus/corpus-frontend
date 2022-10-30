343fe2f1ad52dc98be2fb3879595361770a7f772
Retr0327 <philcoke35@gmail.com>
2022-10-30 16:20:59 +0800

    Add title and meta to Head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,31 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import { getCookie } from 'cookies-next';
+import Header from '@components/UI/Header';
+import { ColorScheme } from '@mantine/core';
+import { GetServerSidePropsContext } from 'next';
+import DarkThemeContext from '@contexts/DarkTheme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps, colorScheme } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Taiwan Social Media Corpus</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <DarkThemeContext colorScheme={colorScheme}>
+        <Header />
+        <Component {...pageProps} />
+      </DarkThemeContext>
+    </>
+  );
 }
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+});
+
+export default App;
