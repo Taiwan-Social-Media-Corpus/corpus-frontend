0e65d2cdb7efe2b8a374718c6822d8851e4e4146
Retr0327 <philcoke35@gmail.com>
2022-10-30 14:14:10 +0800

    Add `version` style

--- src/components/UI/Header/Header.styles.ts
+++ src/components/UI/Header/Header.styles.ts
@@ -18,6 +18,17 @@ const useStyles = createStyles((theme) => ({
       display: 'none',
     },
   },
+
+  version: {
+    ...theme.fn.focusStyles(),
+    fontWeight: 700,
+    textDecoration: 'none',
+    marginTop: 2,
+
+    [theme.fn.smallerThan(860)]: {
+      display: 'none',
+    },
+  },
 }));
 
 export default useStyles;
