9eeb61af80c9c5950a6bdb554b31d3de81b1bfe7
Retr0327 <philcoke35@gmail.com>
2022-11-05 12:55:35 +0800

    Add useStyles

---
+++ src/components/Corpus/Concordance/ErrorCQL/ErrorCQL.styles.ts
@@ -0,0 +1,43 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+    [theme.fn.smallerThan('sm')]: {
+      paddingTop: 20,
+      paddingBottom: 40,
+    },
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: 34,
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 32,
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default useStyles;
