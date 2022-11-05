aed6eb62f15191c561a8b28ddbc57c88b65e2460
Retr0327 <philcoke35@gmail.com>
2022-11-06 00:39:07 +0800

    Use `ErrorPage` as template

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,64 +1,21 @@
+import Link from 'next/link';
 import { memo } from 'react';
-import { createStyles, Title, Text, Button, Container, Group } from '@mantine/core';
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    paddingTop: 180,
-    paddingBottom: 80,
-
-    [theme.fn.smallerThan('sm')]: {
-      paddingTop: 140,
-    },
-  },
-
-  label: {
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: 220,
-    lineHeight: 1,
-    marginBottom: theme.spacing.xl * 1.5,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4],
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 120,
-    },
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: 38,
-
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 32,
-    },
-  },
-
-  description: {
-    maxWidth: 500,
-    margin: 'auto',
-    marginTop: theme.spacing.xl,
-    marginBottom: theme.spacing.xl * 1.5,
-  },
-}));
+import Route from '@config/routes';
+import { Button } from '@mantine/core';
+import ErrorPage from '@components/ErrorPage';
 
 function NotFoundTitle() {
-  const { classes } = useStyles();
-
   return (
-    <Container className={classes.root}>
-      <div className={classes.label}>404</div>
-      <Title className={classes.title}>You have found a secret place.</Title>
-      <Text color="dimmed" size="lg" align="center" className={classes.description}>
-        Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
-        been moved to another URL.
-      </Text>
-      <Group position="center">
-        <Button component="a" href="/" variant="outline" size="md">
+    <ErrorPage
+      code={404}
+      title="You have found a secret place."
+      description="Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL."
+      button={
+        <Button variant="outline" component={Link} href={Route.HOME} size="md">
           Take me back to home page
         </Button>
-      </Group>
-    </Container>
+      }
+    />
   );
 }
 
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,69 +1,23 @@
+import { memo } from 'react';
+import { Button } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { createStyles, Title, Text, Button, Container, Group } from '@mantine/core';
-
-const useStyles = createStyles((theme) => ({
-  root: {
-    paddingTop: 180,
-    paddingBottom: 80,
-
-    [theme.fn.smallerThan('sm')]: {
-      paddingTop: 140,
-    },
-  },
-
-  label: {
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: 220,
-    lineHeight: 1,
-    marginBottom: theme.spacing.xl * 1.5,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4],
-
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 120,
-    },
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: 38,
-
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: 32,
-    },
-  },
-
-  description: {
-    maxWidth: 540,
-    margin: 'auto',
-    marginTop: theme.spacing.xl,
-    marginBottom: theme.spacing.xl * 1.5,
-  },
-}));
+import ErrorPage from '@components/ErrorPage';
 
 function ServerError() {
-  const { classes } = useStyles();
   const router = useRouter();
 
   return (
-    <div className={classes.root}>
-      <Container>
-        <div className={classes.label}>500</div>
-        <Title className={classes.title}>Something bad just happened...</Title>
-        <Text size="lg" align="center" className={classes.description}>
-          Our servers could not handle your request. Don&apos;t worry, our development team was
-          already notified. Try refreshing the page.
-        </Text>
-        <Group position="center">
-          <Button variant="outline" size="md" onClick={() => router.push(router.asPath)}>
-            Refresh the page
-          </Button>
-        </Group>
-      </Container>
-    </div>
+    <ErrorPage
+      code={500}
+      title="Something bad just happened..."
+      description="Our servers could not handle your request. Don't worry, our development team was already notified. Try refreshing the page."
+      button={
+        <Button variant="outline" size="md" onClick={() => router.push(router.asPath)}>
+          Refresh the page
+        </Button>
+      }
+    />
   );
 }
 
-export default ServerError;
+export default memo(ServerError);
