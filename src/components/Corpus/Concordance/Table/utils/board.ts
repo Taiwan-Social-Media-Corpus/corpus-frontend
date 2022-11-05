a18814c790f8b670eacb609a1d3d3021d735b2fd
Retr0327 <philcoke35@gmail.com>
2022-11-05 18:09:54 +0800

    Add createLink

--- src/components/Corpus/Concordance/Table/utils/board.ts
+++ src/components/Corpus/Concordance/Table/utils/board.ts
@@ -3,4 +3,11 @@ function getBoard(media: string, board: string) {
   return board.match(match)![0];
 }
 
-export default getBoard;
+function createLink(media: string, docId: string, board: string) {
+  const filteredBoard = getBoard(media, board);
+
+  if (media === 'ptt') return `https://www.ptt.cc/bbs/${filteredBoard}/${docId}.html`;
+  return `https://www.dcard.tw/f/${filteredBoard}/p/${docId}`;
+}
+
+export { getBoard, createLink };
