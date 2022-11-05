5ba056387c9b0a838aafae7dd1dbbb8070d5e5f1
Retr0327 <philcoke35@gmail.com>
2022-11-05 12:07:19 +0800

    Add getConcordance

---
+++ src/services/concordance/index.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+import API from '@config/api';
+import request from '@utils/request';
+import { Response, RequestBody, BlacklabResponse } from 'types';
+
+type ResponseType = Response<BlacklabResponse> | null;
+
+const getConcordance = (payload: RequestBody) => {
+  const { data, error, mutate } = useSWR<ResponseType>(API.corpus, (url) =>
+    request({ url, method: 'POST', payload })
+  );
+
+  return { concordance: data, isLoading: !error && !data, isError: error, mutate };
+};
+
+export default getConcordance;
