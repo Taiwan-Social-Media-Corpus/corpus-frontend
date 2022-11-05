78ea0c29664175fa1d90ee97c40a1d8e1b852f4a
Retr0327 <philcoke35@gmail.com>
2022-11-05 12:07:58 +0800

    Update `boards` return value

--- src/utils/url/query/index.ts
+++ src/utils/url/query/index.ts
@@ -1,25 +1,22 @@
 import { RequestBody } from 'types';
 
-function getTablePayload(page: string, e: string) {
-  try {
-    const decoded = decodeURIComponent(window.atob(e));
-    const params = new URLSearchParams(decoded);
+function getConcordancePayload(page: string, e: string) {
+  const decoded = decodeURIComponent(Buffer.from(e, 'base64').toString('ascii'));
+  const params = new URLSearchParams(decoded);
+  const boardsParam = params.get('b');
 
-    return {
-      word: params.get('w'),
-      media: params.get('m') === 'all' ? '' : params.get('m'),
-      cqlEnable: params.get('c') === 'true',
-      postType: params.get('p') === 'all' ? '' : params.get('p'),
-      boards: params.get('b')?.split(','),
-      start: params.get('s'),
-      end: params.get('e'),
-      windowSize: params.get('win'),
-      page: Number(page),
-      fetchNumber: Number(params.get('f')),
-    } as RequestBody;
-  } catch (error) {
-    return false;
-  }
+  return {
+    word: params.get('w'),
+    media: params.get('m') === 'all' ? '' : params.get('m'),
+    cqlEnable: params.get('c') === 'true',
+    postType: params.get('p') === 'all' ? '' : params.get('p'),
+    boards: !boardsParam ? null : boardsParam.split(','),
+    start: params.get('s'),
+    end: params.get('e'),
+    windowSize: params.get('win'),
+    page: Number(page),
+    fetchNumber: Number(params.get('f')),
+  } as RequestBody;
 }
 
-export default getTablePayload;
+export default getConcordancePayload;
