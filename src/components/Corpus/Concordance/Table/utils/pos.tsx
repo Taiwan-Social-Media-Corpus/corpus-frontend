d10936c02ff28e7a42c82b945cbdaa06f9ad4522
Retr0327 <philcoke35@gmail.com>
2022-11-05 18:13:40 +0800

    Add createPos

--- src/components/Corpus/Concordance/Table/utils/pos.tsx
+++ src/components/Corpus/Concordance/Table/utils/pos.tsx
@@ -1,4 +1,6 @@
-export function createPosColor(pos: string) {
+import { Text } from '@mantine/core';
+
+function createPosColor(pos: string) {
   if (pos.length > 4) return 'rgb(102, 102, 102)';
 
   const posColorFactories: { [key in string]: any } = {
@@ -16,3 +18,18 @@ export function createPosColor(pos: string) {
 
   return posColorFactories[pos[0]];
 }
+
+const zip = (...rows: any[]): string[][] => [...rows[0]].map((_, c) => rows.map((row) => row[c]));
+
+function createPos(wordArray: string[], posArray: string[]) {
+  return zip(wordArray, posArray).map((value, index) => (
+    <Text component="span" key={`${value}-${index}`} weight={500} ml="auto" mr="auto">
+      {value[0]}
+      <sub style={{ marginRight: '0.7rem', marginLeft: '0.3rem', color: createPosColor(value[1]) }}>
+        {value[1]}
+      </sub>
+    </Text>
+  ));
+}
+
+export default createPos;
