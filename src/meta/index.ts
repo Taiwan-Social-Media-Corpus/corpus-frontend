d172a0bb25650e75336902cd8136d336cb99de83
Retr0327 <philcoke35@gmail.com>
2022-11-13 01:17:46 +0800

    Add meta

---
+++ src/meta/index.ts
@@ -0,0 +1,23 @@
+const meta = {
+  docsLink: '/',
+
+  facebook: {
+    link: 'https://www.facebook.com/groups/174362969268953',
+    color: '#1778F2',
+  },
+
+  github: {
+    lopen: 'https://github.com/lopentu',
+    organization: 'https://github.com/Taiwan-Social-Media-Corpus',
+    discussions: {
+      backend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-backend/discussions',
+      frontend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-frontend/discussions',
+    },
+    releases: {
+      backend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-backend/releases',
+      frontend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-frontend/releases',
+    },
+  },
+};
+
+export default meta;
