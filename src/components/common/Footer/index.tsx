f75be3588a6d210d438216978dff49b81b673e14
Retr0327 <philcoke35@gmail.com>
2022-11-13 02:30:19 +0800

    Update `SocialButtons` import path

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -7,11 +7,9 @@ import { FOOTER_LINKS_DATA } from './LinksGroup';
 
 const LinksGroup = dynamic(() => import('./LinksGroup').then((module) => module.LinksGroup));
 const FacebookButton = dynamic(() =>
-  import('@components/common/SocialButtons').then((module) => module.FacebookButton)
+  import('./SocialButtons').then((module) => module.FacebookButton)
 );
-const GithubButton = dynamic(() =>
-  import('@components/common/SocialButtons').then((module) => module.GithubButton)
-);
+const GithubButton = dynamic(() => import('./SocialButtons').then((module) => module.GithubButton));
 
 type Props = { withNavbar?: boolean };
 
