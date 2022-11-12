165bd6292aefefd90386b81230d2591b95b962f4
Retr0327 <philcoke35@gmail.com>
2022-11-12 16:46:16 +0800

    Update return data

--- src/services/concordance/index.ts
+++ src/services/concordance/index.ts
@@ -10,7 +10,7 @@ const getConcordance = (payload: RequestBody) => {
     request({ url, method: 'POST', payload })
   );
 
-  return { concordance: data, isLoading: !error && !data, isError: error, mutate };
+  return { data, isLoading: !error && !data, isError: error, mutate };
 };
 
 export default getConcordance;
