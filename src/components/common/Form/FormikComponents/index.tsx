9b8e0049c066fa20e08d2a2068838e2ef23524da
Retr0327 <philcoke35@gmail.com>
2022-11-12 17:16:56 +0800

    Add TextInput

--- src/components/common/Form/FormikComponents/index.tsx
+++ src/components/common/Form/FormikComponents/index.tsx
@@ -1,9 +1,19 @@
 import Switch from './Switch';
 import Select from './Select';
+import TextInput from './TextInput';
 import RadioGroup from './RadioGroup';
 import NumberInput from './NumberInput';
 import MultiSelect from './MultiSelect';
 import CheckboxGroup from './CheckboxGroup';
 import SegmentedControl from './SegmentedControl';
 
-export { Switch, Select, RadioGroup, NumberInput, MultiSelect, CheckboxGroup, SegmentedControl };
+export {
+  Switch,
+  Select,
+  TextInput,
+  RadioGroup,
+  NumberInput,
+  MultiSelect,
+  CheckboxGroup,
+  SegmentedControl,
+};
