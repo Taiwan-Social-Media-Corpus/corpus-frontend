4a8ee232aaf3bb74cd373ed2cb3849c736f87a49
Retr0327 <philcoke35@gmail.com>
2022-11-12 17:31:24 +0800

    Rename `doc_id` to `docId`

--- src/types/index.ts
+++ src/types/index.ts
@@ -72,7 +72,7 @@ export interface DocInfos {
     author: string[];
     media: string[];
     title: string[];
-    doc_id: string[];
+    docId: string[];
     board: string[];
     lengthInTokens: number;
     mayView: boolean;
@@ -124,7 +124,7 @@ export interface DocFields {
 export interface MetadataFieldDisplayNames {
   author: string;
   board: string;
-  doc_id: string;
+  docId: string;
   fromInputFile: string;
   media: string;
   title: string;
