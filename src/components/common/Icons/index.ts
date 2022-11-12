82d7709fcacccc319f366e961da65b1b6b9c86bb
Retr0327 <philcoke35@gmail.com>
2022-11-12 17:22:47 +0800

    Add PttIcon

--- src/components/common/Icons/index.ts
+++ src/components/common/Icons/index.ts
@@ -1,5 +1,6 @@
+import PttIcon from './Ptt';
 import DcardIcon from './Dcard';
 import GithubIcon from './Github';
 import FacebookIcon from './Facebook';
 
-export { DcardIcon, GithubIcon, FacebookIcon };
+export { PttIcon, DcardIcon, GithubIcon, FacebookIcon };
