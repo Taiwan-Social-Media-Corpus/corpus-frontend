4c6f3e220d508dc7307b56abdaa4cf1887ccac7b
Retr0327 <philcoke35@gmail.com>
2022-11-12 16:54:16 +0800

    Add ConcordanceProvider

--- src/contexts/Concordance/index.tsx
+++ src/contexts/Concordance/index.tsx
@@ -1,8 +1,49 @@
-import { createContext, useContext } from 'react';
-import { ConcordanceContextType } from './types';
+import { createContext, useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { ConcordancePageProps } from 'types';
+import getConcordance from '@services/concordance';
+import Action from './actions';
+import useConcordanceReducer from './useConcordanceReducer';
+import { ConcordanceContextType, ContextProviderProps } from './types';
 
 const ConcordanceContext = createContext({} as ConcordanceContextType);
 
 const useConcordance = () => useContext(ConcordanceContext);
 
-export default useConcordance;
+const ConcordanceProvider = (props: ContextProviderProps & ConcordancePageProps) => {
+  const { children, payload } = props;
+  const router = useRouter();
+  const [concordanceData, dispatch] = useConcordanceReducer();
+  const { data, isError, isLoading, mutate } = getConcordance(payload);
+
+  useEffect(() => {
+    if (isLoading) {
+      return dispatch({ type: Action.PENDING });
+    }
+
+    if (isError || !data || data.msg === 'internal server error') {
+      dispatch({ type: Action.FETCH_ERROR, payload: 'internal server error' });
+      router.push('/500', { pathname: router.pathname });
+      return undefined;
+    }
+
+    if (data.status === 'failed') {
+      switch (data.msg) {
+        case 'no hits':
+          return dispatch({ type: Action.FETCH_ERROR, payload: 'no hits' });
+        default:
+          return dispatch({ type: Action.FETCH_ERROR, payload: data.msg as string });
+      }
+    }
+
+    return dispatch({ type: Action.FETCH_SUCCESS, payload: data.data });
+  }, [data]);
+
+  return (
+    <ConcordanceContext.Provider value={{ concordance: concordanceData, dispatch, mutate }}>
+      {children}
+    </ConcordanceContext.Provider>
+  );
+};
+
+export { useConcordance, ConcordanceProvider };
