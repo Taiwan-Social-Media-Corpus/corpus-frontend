b33e9b88c680ec3d22d5d5ecea312408e51b8979
Retr0327 <philcoke35@gmail.com>
2022-11-12 17:35:34 +0800

    Add CorpusFormProps

--- src/components/Corpus/Form/types.ts
+++
@@ -1,9 +0,0 @@
-import { Boards, RequestBody } from 'types';
-
-export type CorpusFormProps = { boards: Boards };
-
-export type FormValues = Omit<RequestBody, 'page' | 'start' | 'end' | 'windowSize'> & {
-  start: number;
-  end: number;
-  windowSize: number;
-};
