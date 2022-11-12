ea93575ae5f8e7873e8c94079948bfa263598c90
Retr0327 <philcoke35@gmail.com>
2022-11-12 17:15:47 +0800

    Add SegmentedControl

--- src/components/common/Form/FormikComponents/index.tsx
+++ src/components/common/Form/FormikComponents/index.tsx
@@ -2,5 +2,6 @@ import RadioGroup from './RadioGroup';
 import NumberInput from './NumberInput';
 import MultiSelect from './MultiSelect';
 import CheckboxGroup from './CheckboxGroup';
+import SegmentedControl from './SegmentedControl';
 
-export { RadioGroup, NumberInput, MultiSelect, CheckboxGroup };
+export { RadioGroup, NumberInput, MultiSelect, CheckboxGroup, SegmentedControl };
