a9378a47e65795be9cb2bf4963d3aebd4a011947
Retr0327 <philcoke35@gmail.com>
2022-11-19 13:04:05 +0800

    Add LopenIcon

--- src/components/common/Icons/index.ts
+++ src/components/common/Icons/index.ts
@@ -1,6 +1,7 @@
 import PttIcon from './Ptt';
+import LopenIcon from './Lopen';
 import DcardIcon from './Dcard';
 import GithubIcon from './Github';
 import FacebookIcon from './Facebook';
 
-export { PttIcon, DcardIcon, GithubIcon, FacebookIcon };
+export { PttIcon, LopenIcon, DcardIcon, GithubIcon, FacebookIcon };
