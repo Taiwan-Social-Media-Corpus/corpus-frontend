9edeb33e2ee333daf7d5c6e17aa58ee55f0577e5
Retr0327 <philcoke35@gmail.com>
2022-11-19 17:24:53 +0800

    Add useStyles

---
+++ src/components/pages/HomePage/utils/PageSection/PageSection.styles.ts
@@ -0,0 +1,21 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme, { white }: { white: boolean }) => ({
+  wrapper: {
+    paddingTop: 120,
+    paddingBottom: 60,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+  },
+
+  description: {
+    lineHeight: 1.6,
+    opacity: 0.85,
+    color: white || theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+    '@media (max-width: 960px)': {
+      fontSize: theme.fontSizes.md,
+    },
+  },
+}));
+
+export default useStyles;
