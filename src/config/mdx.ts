b273ba56b58ba64e6dfcf7629d9d6941dbf3cc58
Retr0327 <philcoke35@gmail.com>
2022-11-20 12:21:56 +0800

    Remove autolink headings setting

--- src/config/mdx.ts
+++ src/config/mdx.ts
@@ -2,25 +2,12 @@ import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypePrism from 'rehype-prism-plus';
 import rehypeCodeTitles from 'rehype-code-titles';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { SerializeOptions } from 'next-mdx-remote/dist/types';
 
 const serializeConfig = (data: { [key: string]: any }): SerializeOptions => ({
   mdxOptions: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeCodeTitles,
-      rehypePrism,
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            className: ['anchor'],
-          },
-        },
-      ],
-    ],
+    rehypePlugins: [rehypeSlug, rehypeCodeTitles, rehypePrism],
     format: 'mdx',
   },
   scope: data,
