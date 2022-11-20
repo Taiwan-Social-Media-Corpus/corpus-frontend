3e4f404928a877000f88c11b29923c0fc103f51f
Retr0327 <philcoke35@gmail.com>
2022-11-20 13:53:58 +0800

    Remove prism config

--- src/config/mdx.ts
+++ src/config/mdx.ts
@@ -1,13 +1,12 @@
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
-import rehypePrism from 'rehype-prism-plus';
 import rehypeCodeTitles from 'rehype-code-titles';
 import { SerializeOptions } from 'next-mdx-remote/dist/types';
 
 const serializeConfig = (data: { [key: string]: any }): SerializeOptions => ({
   mdxOptions: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeSlug, rehypeCodeTitles, rehypePrism],
+    rehypePlugins: [rehypeSlug, rehypeCodeTitles],
     format: 'mdx',
   },
   scope: data,
