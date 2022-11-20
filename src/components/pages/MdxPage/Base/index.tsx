b79960d10362747d3472b08ed45681ad59829a7a
Retr0327 <philcoke35@gmail.com>
2022-11-20 17:45:41 +0800

    Add MdxPageBase

---
+++ src/components/pages/MdxPage/Base/index.tsx
@@ -0,0 +1,18 @@
+import { memo } from 'react';
+import dynamic from 'next/dynamic';
+import useStyles from './Base.styles';
+
+const Footer = dynamic(() => import('@components/common/Footer'));
+
+function MdxPageBase({ children }: { children: React.ReactNode }) {
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <div className={classes.content}>{children}</div>
+      <Footer withNavbar />
+    </>
+  );
+}
+
+export default memo(MdxPageBase);
