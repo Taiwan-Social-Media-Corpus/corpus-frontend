e2b3f8e9647b250877a1c714446bd3ecdee5ac54
Retr0327 <philcoke35@gmail.com>
2022-11-20 14:36:28 +0800

    Add mdx parameters

--- src/meta/index.ts
+++ src/meta/index.ts
@@ -20,7 +20,13 @@ const meta = {
   },
 
   layout: {
-    mdx: { breakpoint: 1080 },
+    mdx: {
+      breakpoint: 1080,
+      contentWidth: 820,
+      tableOfContentsWidth: 260,
+      tabHeight: 46,
+      tabHeightMobile: 38,
+    },
     header: { height: 60 },
     navbar: { width: 260, breakpoint: 760 },
     footer: { height: 400, heightTablet: 460, heightMobile: 320 },
