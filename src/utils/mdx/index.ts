df2f1b9250bc2cd18077bc7fb3958f319951b60a
Retr0327 <philcoke35@gmail.com>
2022-11-25 19:45:28 +0800

    Update return fields naming

--- src/utils/mdx/index.ts
+++ src/utils/mdx/index.ts
@@ -15,9 +15,9 @@ async function getHeadings(content: string) {
   const headingLines = content.split('\n').filter((line) => line.match(/^###*\s/));
 
   return headingLines.map((raw) => {
-    const text = raw.replace(/^###*\s/, '');
-    const level = raw.slice(0, 3) === '###' ? 3 : 2;
-    return { text, level };
+    const value = raw.replace(/^###*\s/, '');
+    const depth = raw.slice(0, 3) === '###' ? 3 : 2;
+    return { value, depth };
   });
 }
 
