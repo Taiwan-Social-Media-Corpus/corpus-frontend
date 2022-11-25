434aed080d4632ef4bf8585e63d945f1231bc7c8
Retr0327 <philcoke35@gmail.com>
2022-11-25 19:34:32 +0800

    Remove `rehypeCodeTitles` plugin

--- src/config/mdx.ts
+++ src/config/mdx.ts
@@ -1,12 +1,11 @@
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
-import rehypeCodeTitles from 'rehype-code-titles';
 import { SerializeOptions } from 'next-mdx-remote/dist/types';
 
 const serializeConfig = (data: { [key: string]: any }): SerializeOptions => ({
   mdxOptions: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeSlug, rehypeCodeTitles],
+    rehypePlugins: [rehypeSlug],
     format: 'mdx',
   },
   scope: data,
