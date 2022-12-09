05d5e53d4f55d53e96bd5f69d9366f6ada0ee027
Retr0327 <philcoke35@gmail.com>
2022-12-10 01:30:33 +0800

    Rename `getPostFilesPath` to `getPostDir`

--- src/utils/mdx/path/index.ts
+++ src/utils/mdx/path/index.ts
@@ -6,7 +6,7 @@ type Page = 'about' | 'guide';
 const getPostDirPath = (page: Page) =>
   path.join(process.cwd(), `src/components/pages/${page.charAt(0).toUpperCase() + page.slice(1)}`);
 
-const getPostFilesPath = (page: Page) => {
+const getPostDir = (page: Page) => {
   const postPath = getPostDirPath(page);
   const postFilePaths = fs.readdirSync(postPath).filter((file) => /\.mdx?$/.test(file));
 
@@ -15,4 +15,4 @@ const getPostFilesPath = (page: Page) => {
     .map((slug) => ({ params: { slug } }));
 };
 
-export { getPostDirPath, getPostFilesPath };
+export { getPostDirPath, getPostDir };
