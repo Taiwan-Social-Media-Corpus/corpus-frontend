5a52c8d510a8a583282566d36d59e719d021e9e7
Retr0327 <philcoke35@gmail.com>
2022-12-22 17:08:18 +0800

    Update button href

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,7 +13,7 @@ function NotFoundTitle() {
       title="You have found a secret place."
       description="Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL."
       button={
-        <Button variant="outline" component={Link} href={Route.HOME} size="md">
+        <Button variant="outline" component={Link} href={Route.home} size="md">
           Take me back to home page
         </Button>
       }
