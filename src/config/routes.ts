2117356654f84c8af29e452318c2c887f9161366
Retr0327 <philcoke35@gmail.com>
2022-12-22 16:20:16 +0800

    Update routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,27 @@
+import urlJoin from 'url-join';
+
+const recovery = '/recovery';
+const corpus = '/corpus';
+
 const Route = {
-  HOME: '/',
-  ABOUT: '/about',
-  GUIDE: '/guide',
-  CORPUS: '/corpus',
-  CONCORDANCE: '/corpus/concordance',
+  home: '/',
+  about: '/about',
+  guide: '/guide',
+  login: '/login',
+  logout: '/logout',
+  signUp: '/signup',
+  activation: '/activation',
+  recovery: {
+    root: recovery,
+    validation: urlJoin(recovery, 'validation'),
+    reset: urlJoin(recovery, 'reset'),
+  },
+  corpus: {
+    root: corpus,
+    concordance: urlJoin(corpus, '/concordance'),
+  },
+  notFound: '/404',
+  serverError: '/500',
 } as const;
 
 export default Route;
