a9a0c7f61c3942f9e967c13a7e569c7923c9f18c
Retr0327 <philcoke35@gmail.com>
2022-12-22 16:13:42 +0800

    Update API urls

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,20 +1,59 @@
 import urlJoin from 'url-join';
 
-const domain = process.env.NEXT_PUBLIC_API_SERVICE;
-const blacklabDomain = process.env.NEXT_PUBLIC_BLACKLAB_URL;
+// ----- version tag -----
+const V1 = 'v1';
 
-if (!domain) {
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_URL) {
   throw new Error('API_SERVICE undefined');
 }
 
-if (!blacklabDomain) {
-  throw new Error('BLACKLAB_URL undefined');
-}
+const api = process.env.NODE_ENV === 'production' ? `/api/${V1}` : API_URL;
 
-const api = process.env.NODE_ENV === 'production' ? '/api/v1' : domain;
+// ----- user service -----
+const user = urlJoin(api, 'user');
+const activation = urlJoin(user, 'activation');
+const recovery = urlJoin(user, 'recovery');
+const email = urlJoin(user, 'email');
+
+// ---- corpus service ----
 
 const corpus = urlJoin(api, 'corpus');
 
-const API = { corpus, boards: urlJoin(corpus, 'boards') } as const;
+const API = {
+  V1: {
+    user: {
+      root: user,
+      email: {
+        root: email,
+        validate: urlJoin(email, 'validate'),
+        resend: urlJoin(email, 'resend'),
+      },
+      password: urlJoin(user, 'password'),
+      session: urlJoin(user, 'session'),
+      activation: {
+        root: activation,
+        email: urlJoin(activation, 'email'),
+        resend: urlJoin(activation, 'resend'),
+      },
+      recovery: {
+        root: recovery,
+        password: urlJoin(recovery, 'password'),
+        code: urlJoin(recovery, 'code'),
+        resend: urlJoin(recovery, 'resend'),
+      },
+    },
+    corpus: {
+      root: corpus,
+      media: urlJoin(corpus, 'media'),
+      boards: urlJoin(corpus, 'boards'),
+      concordance: {
+        root: urlJoin(corpus, 'concordance'),
+        csv: urlJoin(corpus, 'concordance', 'csv'),
+      },
+    },
+  },
+};
 
-export { API, blacklabDomain };
+export default API;
