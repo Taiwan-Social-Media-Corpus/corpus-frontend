499bde8ae728d2e474a886a04ceab6cf8f8042ed
Retr0327 <philcoke35@gmail.com>
2022-12-23 16:15:17 +0800

    Add concordance request service

---
+++ src/services/corpus/concordance/get.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+import API from '@config/api';
+import { Response } from 'types';
+import request from '@utils/request';
+import { Concordance, ConcordanceRequestBody } from 'types/corpus';
+
+type ResponseType = Response<Concordance> | null;
+
+const useConcordance = (payload: ConcordanceRequestBody) => {
+  const { data, error, mutate } = useSWR<ResponseType>(API.V1.corpus.concordance.root, (url) =>
+    request({ url, method: 'POST', payload })
+  );
+  return { concordance: data, isLoading: !error && !data, isError: error, mutate };
+};
+
+export default useConcordance;
