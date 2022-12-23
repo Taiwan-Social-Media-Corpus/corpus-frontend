ae9dd70ccb21bd0307cc90fa4affc0f3a92878c3
Retr0327 <philcoke35@gmail.com>
2022-12-24 01:48:31 +0800

    Add Footer

---
+++ src/components/common/ui/Footer/index.tsx
@@ -0,0 +1,66 @@
+import dynamic from 'next/dynamic';
+import { memo, useMemo } from 'react';
+import { Container, Text, Group } from '@mantine/core';
+import IconController from '@components/common/ui/Icons';
+import useStyles from './Footer.styles';
+import { links } from './LinksGroup';
+
+const LinksGroup = dynamic(() => import('./LinksGroup'));
+const FacebookButton = dynamic(() =>
+  import('./SocialButtons').then((module) => module.FacebookButton)
+);
+const GithubButton = dynamic(() => import('./SocialButtons').then((module) => module.GithubButton));
+
+type Props = { withNavbar?: boolean };
+
+function Footer(props: Props) {
+  const { withNavbar } = props;
+  const { classes, cx } = useStyles();
+  const groups = useMemo(
+    () =>
+      links.map((group, index) => (
+        <LinksGroup key={`${group.title}-${index}`} data={group.data} title={group.title} />
+      )),
+    [links]
+  );
+
+  return (
+    <>
+      <div className={classes.spacer} />
+      <div className={cx(classes.wrapper, { [classes.withNavbar]: withNavbar })}>
+        <Container size={1100}>
+          <div className={classes.inner}>
+            <div className={classes.logoSection}>
+              <Group>
+                <IconController control="lopen" size={80} renderType="footer" />
+                <Text ff="Monaco,Courier,monospace" size="md" weight={600}>
+                  LOPEN
+                </Text>
+              </Group>
+              <Text className={classes.description} size="sm">
+                Build fully functional accessible corpus faster than ever
+              </Text>
+            </div>
+
+            <div className={classes.groups}>{groups}</div>
+          </div>
+
+          <div className={classes.afterFooter}>
+            <Group position="apart">
+              <Text size="xs" className={classes.afterFooterNote}>
+                Built by <a href="https://github.com/Retr0327">Lixing Yang</a> and{' '}
+                <a href="https://github.com/loperntu">Shu-Kai Hsieh</a>
+              </Text>
+              <div className={classes.social}>
+                <FacebookButton className={classes.socialButton} />
+                <GithubButton className={classes.socialButton} ml="md" />
+              </div>
+            </Group>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default memo(Footer);
