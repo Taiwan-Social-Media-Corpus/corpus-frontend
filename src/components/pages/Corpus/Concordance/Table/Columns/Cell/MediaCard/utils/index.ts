4e7c0fb7a0c06e0fc10b9788c6e8af5aa82008bf
Retr0327 <philcoke35@gmail.com>
2022-12-26 17:33:23 +0800

    Add createLink for generating media links

--- src/components/pages/Corpus/Concordance/Table/Columns/Cell/MediaCard/utils/index.ts
+++ src/components/pages/Corpus/Concordance/Table/Columns/Cell/MediaCard/utils/index.ts
@@ -2,4 +2,12 @@ function getBoard(media: string, board: string) {
   return media === 'ptt' ? board.replace(/-ptt$/, '') : board.replace(/-dcard$/, '');
 }
 
-export default getBoard;
+function createLink(media: string, board: string, docId: string) {
+  const filteredBoard = getBoard(media, board);
+
+  return media === 'ptt'
+    ? `https://www.ptt.cc/bbs/${filteredBoard}/${docId}.html`
+    : `https://www.dcard.tw/f/${filteredBoard}/p/${docId}`;
+}
+
+export { getBoard, createLink };
