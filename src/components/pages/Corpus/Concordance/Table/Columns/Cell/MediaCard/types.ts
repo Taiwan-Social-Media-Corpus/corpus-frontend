39926bc621091e9df4943b064b92f9bc1d791132
Retr0327 <philcoke35@gmail.com>
2022-12-26 17:42:40 +0800

    Add MediaCardProps

--- src/components/pages/Corpus/Concordance/Table/Columns/Cell/MediaCard/types.ts
+++ src/components/pages/Corpus/Concordance/Table/Columns/Cell/MediaCard/types.ts
@@ -1,5 +1,12 @@
+import { DocInfos } from 'types/corpus';
+
 export type PostLinkProps = {
   media: string;
   board: string;
   docId: string;
+};
+
+export type MediaCardProps = {
+  docInfos: DocInfos;
+  docPid: string;
 };
