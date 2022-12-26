75b08fc4b55be21f68d66bd0a46e2bcdc037b6d5
Retr0327 <philcoke35@gmail.com>
2022-12-26 20:10:06 +0800

    Add buildOptions for constructing query options

--- src/services/corpus/concordance/get.ts
+++ src/services/corpus/concordance/get.ts
@@ -1,16 +1,28 @@
-import useSWR from 'swr';
 import API from '@config/api';
 import { Response } from 'types';
 import request from '@utils/request';
 import { Concordance, ConcordanceRequestBody } from 'types/corpus';
+import { UseQueryOptions } from '@tanstack/react-query';
 
-type ResponseType = Response<Concordance> | null;
+type ResponseType = Response<Concordance>;
 
-const useConcordance = (payload: ConcordanceRequestBody) => {
-  const { data, error, mutate } = useSWR<ResponseType>(API.V1.corpus.concordance.root, (url) =>
-    request({ url, method: 'POST', payload })
-  );
-  return { concordance: data, isLoading: !error && !data, isError: error, mutate };
-};
+export function buildOptions(payload: ConcordanceRequestBody, prefetch: boolean = false) {
+  const page = prefetch ? payload.page + 1 : payload.page;
 
-export default useConcordance;
+  const options: UseQueryOptions<ResponseType> = {
+    queryKey: ['concordance', page],
+    queryFn: () =>
+      request({
+        url: API.V1.corpus.concordance.root,
+        method: 'POST',
+        payload: { ...payload, page },
+      }),
+  };
+
+  if (!prefetch) {
+    options.keepPreviousData = true;
+    options.staleTime = 5000;
+  }
+
+  return options;
+}
