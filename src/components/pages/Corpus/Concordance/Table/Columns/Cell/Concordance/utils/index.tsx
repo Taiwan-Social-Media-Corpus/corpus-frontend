4cc07743c07752b4d2395c6466c21b9aae53fcaa
Retr0327 <philcoke35@gmail.com>
2022-12-26 17:47:39 +0800

    Add `createPosColor` for generating color codes based on part of speech

---
+++ src/components/pages/Corpus/Concordance/Table/Columns/Cell/Concordance/utils/index.tsx
@@ -0,0 +1,16 @@
+export function createPosColor(pos: string) {
+  const posColorFactories: { [key in string]: any } = {
+    A: 'rgb(21, 170, 191)',
+    C: 'rgb(231, 41, 138)',
+    D: 'rgb(117, 112, 179)',
+    I: 'rgb(102, 166, 30)',
+    N: 'rgb(27, 158, 119)',
+    P: 'rgb(102, 166, 30)',
+    S: 'rgb(166, 118, 29)',
+    T: 'rgb(102, 166, 30)',
+    V: 'rgb(217, 95, 2)',
+    F: 'rgb(230, 171, 2)',
+  };
+
+  return posColorFactories[pos[0]] || 'rgb(102, 102, 102)';
+}
