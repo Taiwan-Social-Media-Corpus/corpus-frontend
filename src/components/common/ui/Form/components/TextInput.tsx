5d55410ef89654cb6fd47f8de67c456beff1fd4f
Retr0327 <philcoke35@gmail.com>
2022-12-27 22:55:54 +0800

    Add Add TextInput

---
+++ src/components/common/ui/Form/components/TextInput.tsx
@@ -0,0 +1,20 @@
+import { TextInputProps } from 'types/mantine';
+import { useController } from 'react-hook-form';
+import { TextInput as MantineTextInput } from '@mantine/core';
+import { ErrorMessage } from './utils';
+
+function TextInput(props: TextInputProps) {
+  const { label, name, ...rest } = props;
+  const {
+    field,
+    fieldState: { error: fieldError },
+  } = useController({ name });
+
+  const error = fieldError ? (
+    <ErrorMessage>{fieldError.message?.toString()}</ErrorMessage>
+  ) : undefined;
+
+  return <MantineTextInput id={name} label={label} error={error} {...rest} {...field} />;
+}
+
+export default TextInput;
