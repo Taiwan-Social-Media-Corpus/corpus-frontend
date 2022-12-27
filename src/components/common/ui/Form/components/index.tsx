cb730a2b1b5d994171e6bb4f905db6a0189febec
Retr0327 <philcoke35@gmail.com>
2022-12-27 23:14:44 +0800

    Add NumberInput

--- src/components/common/ui/Form/components/index.tsx
+++ src/components/common/ui/Form/components/index.tsx
@@ -1,6 +1,7 @@
 import Select from './Select';
 import TextInput from './TextInput';
 import MultiSelect from './MultiSelect';
+import NumberInput from './NumberInput';
 import CheckboxGroup from './CheckboxGroup';
 
-export { Select, TextInput, MultiSelect, CheckboxGroup };
+export { Select, TextInput, MultiSelect, NumberInput, CheckboxGroup };
