54f9db109170d1d333b269a4db0182038a8e9221
Retr0327 <philcoke35@gmail.com>
2022-12-28 00:13:45 +0800

    Add Switch

---
+++ src/components/common/ui/Form/components/Switch.tsx
@@ -0,0 +1,25 @@
+import { SwitchProps } from 'types/mantine';
+import { useController } from 'react-hook-form';
+import { Switch as MantineSwitch } from '@mantine/core';
+
+function Switch(props: SwitchProps) {
+  const { label, name, ...rest } = props;
+  const {
+    field,
+    formState: { defaultValues },
+  } = useController({ name });
+  const { onChange, value: checked, ...restField } = field;
+
+  return (
+    <MantineSwitch
+      id={name}
+      label={label}
+      checked={checked}
+      onChange={(value) => onChange(value ?? defaultValues?.[name])}
+      {...rest}
+      {...restField}
+    />
+  );
+}
+
+export default Switch;
--- src/components/common/ui/Form/components/index.tsx
+++ src/components/common/ui/Form/components/index.tsx
@@ -1,8 +1,9 @@
 import Select from './Select';
+import Switch from './Switch';
 import TextInput from './TextInput';
 import RadioGroup from './RadioGroup';
 import MultiSelect from './MultiSelect';
 import NumberInput from './NumberInput';
 import CheckboxGroup from './CheckboxGroup';
 
-export { Select, TextInput, RadioGroup, MultiSelect, NumberInput, CheckboxGroup };
+export { Select, Switch, TextInput, RadioGroup, MultiSelect, NumberInput, CheckboxGroup };
