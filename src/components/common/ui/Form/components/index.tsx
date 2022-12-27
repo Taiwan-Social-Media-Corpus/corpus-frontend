f1f30a558e2f8a56245becdc4053b516449269d6
Retr0327 <philcoke35@gmail.com>
2022-12-27 23:17:11 +0800

    Add RadioGroup

--- src/components/common/ui/Form/components/index.tsx
+++ src/components/common/ui/Form/components/index.tsx
@@ -1,7 +1,8 @@
 import Select from './Select';
 import TextInput from './TextInput';
+import RadioGroup from './RadioGroup';
 import MultiSelect from './MultiSelect';
 import NumberInput from './NumberInput';
 import CheckboxGroup from './CheckboxGroup';
 
-export { Select, TextInput, MultiSelect, NumberInput, CheckboxGroup };
+export { Select, TextInput, RadioGroup, MultiSelect, NumberInput, CheckboxGroup };
