57f412875288f6c633f92ed7ed64b2c164091d09
Retr0327 <philcoke35@gmail.com>
2022-12-27 23:09:28 +0800

    Add MultiSelect

---
+++ src/components/common/ui/Form/components/MultiSelect.tsx
@@ -0,0 +1,41 @@
+import { useController } from 'react-hook-form';
+import { MultiSelectProps } from 'types/mantine';
+import { MultiSelect as MantineMultiSelect } from '@mantine/core';
+import { ErrorMessage } from './utils';
+
+function MultiSelect(props: MultiSelectProps) {
+  const { label, name, options, ...rest } = props;
+  const {
+    field,
+    fieldState: { error: fieldError },
+    formState: { defaultValues },
+  } = useController({ name });
+
+  const error = fieldError ? (
+    <ErrorMessage>{fieldError.message?.toString()}</ErrorMessage>
+  ) : undefined;
+
+  const { onChange, ...restField } = field;
+
+  return (
+    <MantineMultiSelect
+      label={label}
+      data={options}
+      onChange={(value) => {
+        onChange(value ?? defaultValues?.[value]);
+      }}
+      error={error}
+      getCreateLabel={(query) => `+ ${query}`}
+      onCreate={(query) => {
+        const capitalized = query.charAt(0).toUpperCase() + query.substring(1);
+        const item = { label: capitalized, value: query };
+        options.push(item);
+        return item;
+      }}
+      {...rest}
+      {...restField}
+    />
+  );
+}
+
+export default MultiSelect;
--- src/components/common/ui/Form/components/index.tsx
+++ src/components/common/ui/Form/components/index.tsx
@@ -1,4 +1,5 @@
 import Select from './Select';
 import TextInput from './TextInput';
+import MultiSelect from './MultiSelect';
 
-export { Select, TextInput };
+export { Select, TextInput, MultiSelect };
