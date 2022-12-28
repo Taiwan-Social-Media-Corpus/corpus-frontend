998797b6806ae30d036f2108624f4ebeafa8904d
Retr0327 <philcoke35@gmail.com>
2022-12-29 00:58:08 +0800

    Update SectionTitle props type

--- src/components/pages/Home/Base/Title/index.tsx
+++ src/components/pages/Home/Base/Title/index.tsx
@@ -1,16 +1,14 @@
 import { memo } from 'react';
 import useStyles from './Title.styles';
 
-interface Props extends React.ComponentProps<'h1'> {
-  type?: 'white' | 'default';
-}
+type Props = React.ComponentProps<'h1'>;
 
 function SectionTitle(props: Props) {
-  const { children, className, type = 'default', ...rest } = props;
+  const { children, className, ...rest } = props;
   const { classes, cx } = useStyles();
 
   return (
-    <h2 className={cx(classes.title, classes[type], className)} {...rest}>
+    <h2 className={cx(classes.title, classes.default, className)} {...rest}>
       {children}
     </h2>
   );
