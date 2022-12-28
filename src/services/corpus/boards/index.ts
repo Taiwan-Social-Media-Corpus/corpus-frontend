2399af98da7269adf8f3d4923f51727a0292fd47
Retr0327 <philcoke35@gmail.com>
2022-12-29 00:32:57 +0800

    Migrate codebase from node-fetch to react-query

--- src/services/corpus/boards/index.ts
+++ src/services/corpus/boards/index.ts
@@ -2,16 +2,30 @@ import API from '@config/api';
 import { Response } from 'types';
 import request from '@utils/request';
 import { Boards } from 'types/corpus';
+import { QueryClient, useQuery } from '@tanstack/react-query';
 
 type ResponseType = Response<Boards> | null;
 
-const getBoards = async (): Promise<[ResponseType, any]> => {
-  try {
-    const result = await request({ url: API.V1.corpus.boards, method: 'GET' });
-    return [result, null];
-  } catch (error) {
-    return [null, error];
-  }
+const QUERY_KEY = 'boards';
+
+const fetchBoards = (apiType: 'root' | 'external') => async () => {
+  const url = API.V1.corpus.boards[apiType];
+  return request({ url, method: 'GET' });
 };
 
-export default getBoards;
+const useBoards = () => {
+  const { data, isLoading, isError } = useQuery<ResponseType, any>({
+    queryKey: [QUERY_KEY],
+    queryFn: fetchBoards('root'),
+  });
+
+  return { boards: data, isLoading, isError };
+};
+
+const usePrefetchBoards = async (client: QueryClient) =>
+  client.prefetchQuery({
+    queryKey: [QUERY_KEY],
+    queryFn: fetchBoards('external'),
+  });
+
+export { useBoards, usePrefetchBoards };
