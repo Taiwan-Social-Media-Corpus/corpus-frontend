6292af44eeb116330b5a3d0f8de7a11c40e98e2f
Retr0327 <philcoke35@gmail.com>
2022-12-29 00:59:21 +0800

    Add PageSection

--- src/components/pages/Home/Base/index.tsx
+++ src/components/pages/Home/Base/index.tsx
@@ -1,29 +1,27 @@
 import { memo } from 'react';
 import { Container, Text, DefaultProps, Box } from '@mantine/core';
-import SectionTitle from '../SectionTitle';
+import SectionTitle from './Title';
 import useStyles from './PageSection.styles';
 
-interface PageSectionProps extends DefaultProps {
+interface Props extends DefaultProps {
   title: string;
   description?: string;
   children: React.ReactNode;
-  white?: boolean;
 }
 
-function PageSection(props: PageSectionProps) {
-  const { title, description, children, white, ...rest } = props;
-  const { classes } = useStyles({ white: white as boolean });
+function PageSection(props: Props) {
+  const { title, description, children, ...rest } = props;
+  const { classes } = useStyles();
 
   return (
     <Box className={classes.wrapper} {...rest}>
       <Container size={1100}>
-        <SectionTitle type={white ? 'white' : 'default'}>{title}</SectionTitle>
-        {description && (
+        <SectionTitle>{title}</SectionTitle>
+        {description !== undefined ? (
           <Text className={classes.description} size="xl">
             {description}
           </Text>
-        )}
-
+        ) : null}
         {children}
       </Container>
     </Box>
