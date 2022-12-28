bcc8e4be31ef155e209db7bc73ed088641cb6890
Retr0327 <philcoke35@gmail.com>
2022-12-28 11:51:55 +0800

    Add form options

--- src/components/pages/Corpus/Form/options/index.ts
+++ src/components/pages/Corpus/Form/options/index.ts
@@ -1,3 +1,17 @@
+import { Boards } from 'types/corpus';
 import createPostTypeOptions from './post';
 
-export default createPostTypeOptions;
+const mediaOptions = [
+  { label: 'All', value: 'all' },
+  { label: 'PTT', value: 'ptt', image: '/ptt-logo.png' },
+  { label: 'Dcard', value: 'dcard', image: '/dcard-logo.png' },
+];
+
+function createBoardOptions(boards: Boards, media: string) {
+  return boards[media as keyof Boards].map((value) => ({
+    label: value,
+    value,
+  }));
+}
+
+export { mediaOptions, createBoardOptions, createPostTypeOptions };
