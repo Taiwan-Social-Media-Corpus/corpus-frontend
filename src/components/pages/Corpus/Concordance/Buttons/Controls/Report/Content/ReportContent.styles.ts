ecd09049e5c5c4ca53734254875609a7a525e3c6
Retr0327 <philcoke35@gmail.com>
2022-12-29 16:09:50 +0800

    Add ReportContent styles

---
+++ src/components/pages/Corpus/Concordance/Buttons/Controls/Report/Content/ReportContent.styles.ts
@@ -0,0 +1,25 @@
+import { createStyles } from '@mantine/core';
+
+export const ICON_SIZE = 60;
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    position: 'relative',
+    overflow: 'visible',
+    padding: theme.spacing.lg,
+    paddingTop: theme.spacing.xl * 1.5 + ICON_SIZE / 3,
+  },
+
+  icon: {
+    position: 'absolute',
+    top: -ICON_SIZE / 3,
+    left: `calc(50% - ${ICON_SIZE / 2}px)`,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    lineHeight: 1,
+  },
+}));
+
+export default useStyles;
