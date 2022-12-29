9d7f3596728b02b53d86de755be1e6a81740e1eb
Retr0327 <philcoke35@gmail.com>
2022-12-29 19:33:43 +0800

    Improve performance of subTitle component with useMemo

--- src/components/layout/Corpus/index.tsx
+++ src/components/layout/Corpus/index.tsx
@@ -1,22 +1,32 @@
-import { memo, ReactNode } from 'react';
+import { memo, ReactNode, useMemo } from 'react';
 import { Container, Title, Text, useMantineTheme } from '@mantine/core';
 import useStyles from './Corpus.styles';
 
-type Props = { children: ReactNode; description: string };
+type Props = { children: ReactNode; description: string | ReactNode };
 
 function CorpusLayout(props: Props) {
   const { classes } = useStyles();
   const theme = useMantineTheme();
   const { children, description } = props;
 
+  const subTitle = useMemo(
+    () =>
+      typeof description === 'string' ? (
+        <Text size="sm" className={classes.description}>
+          {description}
+        </Text>
+      ) : (
+        description
+      ),
+    [typeof description]
+  );
+
   return (
     <div className={classes.wrapper}>
       <Container size={700}>
         <Title className={classes.title}>Taiwan Social Media Corpus</Title>
         <Container size={560} p={0}>
-          <Text size="sm" className={classes.description}>
-            {description}
-          </Text>
+          {subTitle}
         </Container>
       </Container>
       <main
