0616a7c7c2176ec913e7a03e8b430090fdf6d244
Retr0327 <philcoke35@gmail.com>
2022-12-29 13:16:39 +0800

    Replace `IconController` with Mantine Avatar

--- src/components/pages/Home/Media/Controls/index.tsx
+++ src/components/pages/Home/Media/Controls/index.tsx
@@ -1,48 +1,43 @@
+import { memo } from 'react';
 import { Media } from 'types/corpus';
-import { memo, useMemo } from 'react';
-import IconController from '@components/common/ui/Icons';
-import { UnstyledButton, Text, SimpleGrid } from '@mantine/core';
+import { UnstyledButton, Text, SimpleGrid, Avatar } from '@mantine/core';
 import useStyles from './MediaControls.styles';
 
 function MediaControls(props: Media) {
   const { ptt, dcard } = props;
   const { classes } = useStyles();
 
-  const items = useMemo(
-    () =>
-      [
-        {
-          title: 'Ptt',
-          stats: ptt,
-          icon: <IconController control="ptt" height={70} />,
-          href: 'https://www.ptt.cc/bbs/index.html',
-        },
-        {
-          title: 'Dcard',
-          stats: dcard,
-          icon: <IconController control="dcard" height={70} />,
-          href: 'https://www.dcard.tw/f',
-        },
-      ].map((value, index) => (
-        <UnstyledButton
-          component="a"
-          href={value.href}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={classes.control}
-          key={`${value.title}-${index}`}
-        >
-          {value.icon}
-          <Text mt="md" size="sm" sx={{ whiteSpace: 'nowrap' }}>
-            {value.title}
-          </Text>
-          <Text mt={5} size="md" sx={{ whiteSpace: 'nowrap' }}>
-            共 {value.stats} 則貼文
-          </Text>
-        </UnstyledButton>
-      )),
-    [ptt, dcard]
-  );
+  const items = [
+    {
+      title: 'Ptt',
+      stats: ptt,
+      logoSrc: '/ptt-logo.png',
+      href: 'https://www.ptt.cc/bbs/index.html',
+    },
+    {
+      title: 'Dcard',
+      stats: dcard,
+      logoSrc: '/dcard-logo.png',
+      href: 'https://www.dcard.tw/f',
+    },
+  ].map((value, index) => (
+    <UnstyledButton
+      component="a"
+      href={value.href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={classes.control}
+      key={`${value.title}-${index}`}
+    >
+      <Avatar src={value.logoSrc} style={{ height: 80, width: 80 }} />
+      <Text mt="md" size="sm" sx={{ whiteSpace: 'nowrap' }}>
+        {value.title}
+      </Text>
+      <Text mt={5} size="md" sx={{ whiteSpace: 'nowrap' }}>
+        共 {value.stats} 則貼文
+      </Text>
+    </UnstyledButton>
+  ));
 
   return (
     <SimpleGrid
