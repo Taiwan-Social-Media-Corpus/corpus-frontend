24565d7a3fddefeb32e66ee9659095cf41d335f0
Retr0327 <philcoke35@gmail.com>
2022-12-30 18:12:05 +0800

    Add AlertAction

---
+++ src/contexts/Alert/actions.ts
@@ -0,0 +1,10 @@
+enum AlertAction {
+  RESET = 'RESET',
+  RESEND = 'RESEND',
+  EXCESS_EMAIL = 'EXCESS_EMAIL',
+  EXPIRE = 'EXPIRE',
+  ERROR = 'ERROR',
+  EXCESS_ERROR = 'EXCESS_ERROR',
+}
+
+export default AlertAction;
