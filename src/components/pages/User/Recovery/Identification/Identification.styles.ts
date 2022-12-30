83799de4ae8797fede09f053521d21c322192235
Retr0327 <philcoke35@gmail.com>
2022-12-30 18:36:57 +0800

    Add Identification styles

---
+++ src/components/pages/User/Recovery/Identification/Identification.styles.ts
@@ -0,0 +1,19 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  controls: {
+    marginTop: 30,
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column-reverse',
+      marginTop: 32,
+    },
+  },
+  control: {
+    [theme.fn.smallerThan('xs')]: {
+      width: '100%',
+      textAlign: 'center',
+    },
+  },
+}));
+
+export default useStyles;
