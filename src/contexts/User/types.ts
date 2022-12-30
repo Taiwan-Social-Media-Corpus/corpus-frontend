25a62624ab47f1a8265cac0f9f8aed150a8d8c53
Retr0327 <philcoke35@gmail.com>
2022-12-30 12:06:48 +0800

    Add user context related types

---
+++ src/contexts/User/types.ts
@@ -0,0 +1,55 @@
+import { User } from 'types/user';
+import { KeyedMutator } from 'swr';
+import { Optional, Response } from 'types';
+import { Action } from './actions';
+
+type ResData = Pick<User, 'uid' | 'email' | 'firstName' | 'lastName' | 'enabled'>;
+
+export type UserData = Optional<ResData, 'email' | 'firstName' | 'lastName'> & {
+  fullName?: string;
+};
+
+export type InitialState = {
+  pending: boolean;
+  error: string;
+  data: UserData;
+};
+
+type FetchErrorAction = {
+  type: Action.FETCH_ERROR;
+};
+
+type FetchFailedAction = {
+  type: Action.FETCH_FAILED;
+};
+
+type LogoutAction = {
+  type: Action.LOGOUT;
+};
+
+type FetchSuccessAction = {
+  type: Action.FETCH_SUCCESS;
+  payload: UserData;
+};
+
+type PendingAction = {
+  type: Action.PENDING;
+};
+
+export type ActionType =
+  | FetchSuccessAction
+  | LogoutAction
+  | FetchFailedAction
+  | FetchErrorAction
+  | PendingAction;
+
+export type UserContextType = {
+  user: InitialState;
+  mutate: KeyedMutator<any>;
+  dispatch: React.Dispatch<ActionType>;
+  fetchNotification: KeyedMutator<Response<{ count: number }>>;
+};
+
+export type ContextProviderProps = {
+  children: React.ReactNode;
+};
