310a57099b1fe26089934b41b4189f1897d3192a
Retr0327 <philcoke35@gmail.com>
2022-12-30 12:11:56 +0800

    Update ResponseType import path

--- src/services/user/session/create.ts
+++ src/services/user/session/create.ts
@@ -1,11 +1,6 @@
 import API from '@config/api';
-import { Response } from 'types';
-import { User } from 'types/user';
 import request from '@utils/request';
-
-type Payload = Pick<User, 'email' | 'password'>;
-type ResData = Pick<User, 'uid' | 'email' | 'firstName' | 'lastName' | 'enabled'>;
-type ResponseType = Response<ResData> | null;
+import { Payload, ResponseType } from './types';
 
 const createSession = async (payload: Payload): Promise<[ResponseType, any]> => {
   try {
