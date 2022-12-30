0fdb84d31967951a33a6f71c480c850f33b72fce
Retr0327 <philcoke35@gmail.com>
2022-12-30 12:13:22 +0800

    Implement SWR hook for managing user session data

---
+++ src/services/user/session/index.ts
@@ -0,0 +1,14 @@
+import useSWR from 'swr';
+import API from '@config/api';
+import request from '@utils/request';
+import { ResponseType } from './types';
+
+const useSession = () => {
+  const { data, error, mutate } = useSWR<ResponseType>(API.V1.user.session, (url) =>
+    request({ url, method: 'GET' })
+  );
+
+  return { data, isLoading: !error && !data, error, mutate };
+};
+
+export default useSession;
