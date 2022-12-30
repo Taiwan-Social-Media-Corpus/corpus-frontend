f381fc512b141f4300a14bf5c45e6f716186769d
Retr0327 <philcoke35@gmail.com>
2022-12-30 12:08:57 +0800

    Implement user reducer for managing user data

---
+++ src/contexts/User/reducer.tsx
@@ -0,0 +1,30 @@
+import { useReducer } from 'react';
+import { Action } from './actions';
+import { InitialState, ActionType } from './types';
+
+const initialState: InitialState = {
+  pending: true,
+  error: '',
+  data: { uid: '', enabled: false },
+};
+
+const reducer = (state: InitialState, action: ActionType) => {
+  switch (action.type) {
+    case Action.FETCH_ERROR:
+      return { ...state, pending: false, error: 'Oops! Something went wrong!' };
+    case Action.FETCH_SUCCESS:
+      return { ...state, pending: false, data: action.payload };
+    case Action.FETCH_FAILED:
+      return { ...state, pending: false };
+    case Action.PENDING:
+      return { ...state, pending: true };
+    case Action.LOGOUT:
+      return { ...initialState, pending: false };
+    default:
+      return state;
+  }
+};
+
+const useUserReducer = () => useReducer(reducer, initialState);
+
+export default useUserReducer;
