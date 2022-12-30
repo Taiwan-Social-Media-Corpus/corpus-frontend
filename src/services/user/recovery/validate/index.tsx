de4df3ce3b8b392da08d30cd6ed1ceceeaff44c2
Retr0327 <philcoke35@gmail.com>
2022-12-30 17:04:50 +0800

    Implement recovery pin code validation service

---
+++ src/services/user/recovery/validate/index.tsx
@@ -0,0 +1,19 @@
+import API from '@config/api';
+import { Response } from 'types';
+import { User } from 'types/user';
+import request from '@utils/request';
+
+type PayloadType = Pick<User, 'email'> & { code: string };
+type ResData = { errorCount: number };
+type ResponseType = Response<ResData> | null;
+
+const validate = async (payload: PayloadType): Promise<[ResponseType, any]> => {
+  try {
+    const result = await request({ method: 'POST', url: API.V1.user.recovery.code, payload });
+    return [result, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
+
+export default validate;
