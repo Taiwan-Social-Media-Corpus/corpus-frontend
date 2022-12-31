31eadf64751b843b52e462172efb4ddb0cf2e367
Retr0327 <philcoke35@gmail.com>
2022-12-31 12:29:07 +0800

    Remove `ReactNode` type from `description`

--- src/components/layout/Corpus/index.tsx
+++ src/components/layout/Corpus/index.tsx
@@ -2,27 +2,20 @@ import { memo, ReactNode } from 'react';
 import { Container, Title, Text } from '@mantine/core';
 import useStyles from './Corpus.styles';
 
-type Props = { children: ReactNode; description: string | ReactNode };
+type Props = { children: ReactNode; description: string };
 
 function CorpusLayout(props: Props) {
   const { classes } = useStyles();
   const { children, description } = props;
-
-  const subTitle =
-    typeof description === 'string' ? (
-      <Text size="sm" className={classes.description}>
-        {description}
-      </Text>
-    ) : (
-      description
-    );
 
   return (
     <div className={classes.wrapper}>
       <Container size={700}>
         <Title className={classes.title}>Taiwan Social Media Corpus</Title>
         <Container size={560} p={0}>
-          {subTitle}
+          <Text size="sm" className={classes.description}>
+            {description}
+          </Text>
         </Container>
       </Container>
       <main>{children}</main>
