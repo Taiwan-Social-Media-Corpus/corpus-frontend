66f92096789089ecaf82e73f677e79a302e980d1
Retr0327 <philcoke35@gmail.com>
2023-01-05 11:03:54 +0800

    Reframe signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,12 +1,34 @@
 import { NextPage } from 'next';
-import MobileSignUpForm from '@components/pages/User/SignUp/Mobile';
+import { Container, Paper } from '@mantine/core';
 import AuthenticationLayout from '@components/layout/Authentication';
-import DesktopSignUpForm from '@components/pages/User/SignUp/Desktop';
+import MobileSignUpForm from '@components/pages/User/SignUp/Form/Mobile';
+import DesktopSignUpForm from '@components/pages/User/SignUp/Form/Desktop';
 
 const SignUp: NextPage = () => (
   <AuthenticationLayout title="Sign up to LOPEN">
-    <MobileSignUpForm />
-    <DesktopSignUpForm />
+    <Container
+      sx={{
+        '@media (min-width: 420px)': {
+          display: 'none',
+        },
+      }}
+      p={15}
+    >
+      <MobileSignUpForm />
+    </Container>
+
+    <Container
+      size={520}
+      sx={{
+        '@media (max-width: 420px)': {
+          display: 'none',
+        },
+      }}
+    >
+      <Paper withBorder shadow="md" p={30} mt={10} radius="md">
+        <DesktopSignUpForm />
+      </Paper>
+    </Container>
   </AuthenticationLayout>
 );
 
