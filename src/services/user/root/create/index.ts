52e2d88c433c7ccba7a36e2c9202996e2ccfd062
Retr0327 <philcoke35@gmail.com>
2023-01-05 11:17:31 +0800

    Implement user creation service

---
+++ src/services/user/root/create/index.ts
@@ -0,0 +1,18 @@
+import API from '@config/api';
+import { User } from 'types/user';
+import request from '@utils/request';
+import { Response, Optional } from 'types';
+
+type PayloadType = Omit<User, 'uid' | 'enabled'>;
+type ResponseType = Response<Optional<User, 'email'>> | null;
+
+const createUser = async (payload: PayloadType): Promise<[ResponseType, any]> => {
+  try {
+    const result = await request({ method: 'POST', url: API.V1.user.root, payload });
+    return [result, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
+
+export default createUser;
