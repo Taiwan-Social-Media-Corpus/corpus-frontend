1a95f58a24d31373ec76bb5753792074b226bd5e
Retr0327 <philcoke35@gmail.com>
2023-01-07 11:34:53 +0800

    Add Navbar styles

--- src/components/layout/App/Header/Controls/Icons/index.ts
+++
@@ -1,5 +0,0 @@
-import Github from './Github';
-import Facebook from './Facebook';
-import ColorScheme from './ColorScheme';
-
-export { Github, Facebook, ColorScheme };
---
+++ src/components/layout/Navigation/Navbar/Navbar.styles.ts
@@ -0,0 +1,76 @@
+import meta from 'meta';
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme, _params, getRef) => {
+  const icon = getRef('icon');
+
+  return {
+    navbar: {
+      boxSizing: 'border-box',
+      height: '100vh',
+      borderRight: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[2]
+      }`,
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+      position: 'fixed',
+      zIndex: 5,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: meta.layout.navbar.width,
+
+      [`@media (max-width: ${meta.layout.navbar.breakpoint}px)`]: {
+        display: 'none',
+      },
+    },
+
+    mainSection: {
+      paddingRight: theme.spacing.md,
+      paddingBottom: theme.spacing.xl * 2,
+
+      paddingLeft: theme.spacing.md,
+      paddingTop: meta.layout.header.height + theme.spacing.md,
+
+      [`@media (max-width: ${meta.layout.navbar.breakpoint}px)`]: {
+        paddingBottom: 120,
+      },
+    },
+
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      }`,
+    },
+
+    logoutLink: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+        [`& .${icon}`]: {
+          color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        },
+      },
+    },
+
+    logoutIcon: {
+      ref: icon,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+      marginRight: theme.spacing.sm,
+    },
+  };
+});
+
+export default useStyles;
