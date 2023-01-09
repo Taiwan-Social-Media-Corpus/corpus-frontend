1cb997cece115ceaca9a678729718ba17c20211a
Retr0327 <philcoke35@gmail.com>
2023-01-10 01:07:01 +0800

    Pre-render home page by SWR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,39 @@
+import API from '@config/api';
+import { SWRConfig } from 'swr';
 import dynamic from 'next/dynamic';
-import Route from '@config/routes';
-import { Box } from '@mantine/core';
-import { useRouter } from 'next/router';
-import Waves from '@components/pages/Home/Waves';
-import Jumbotron from '@components/pages/Home/Jumbotron';
-import { QueryClient, dehydrate } from '@tanstack/react-query';
-import { useMedia, usePrefetchMedia } from '@services/corpus/media';
-import { useCorpusStats, usePrefetchCorpusStats } from '@services/corpus/stats/corpus';
+import { fetchMedia } from '@services/corpus/media';
+import type { NextPage, GetServerSideProps } from 'next';
+import { fetchCorpusStats } from '@services/corpus/stats/corpus';
 
-const Community = dynamic(() =>
-  import('@components/pages/Home/Community').then((module) => module)
+const HomePage = dynamic(() => import('@components/pages/Home'));
+const Footer = dynamic(() => import('@components/common/ui/Footer'));
+
+type Props = { fallback: { [x: string]: any } };
+
+const Home: NextPage<Props> = (props) => (
+  <SWRConfig value={{ fallback: props.fallback }}>
+    <HomePage />
+    <Footer />
+  </SWRConfig>
 );
-const Media = dynamic(() => import('@components/pages/Home/Media').then((module) => module));
-const Footer = dynamic(() => import('@components/common/ui/Footer').then((module) => module));
-
-function Home() {
-  const router = useRouter();
-  const { media, isMediaError } = useMedia();
-  const { corpusStats, isCorpusStatsError } = useCorpusStats();
-  const isError = isMediaError || isCorpusStatsError || !media || !corpusStats;
-
-  if (isError) {
-    router.push(Route.serverError, { pathname: router.pathname });
-    return null;
-  }
-
-  return (
-    <>
-      <Box sx={(theme) => ({ position: 'relative', zIndex: 1, boxShadow: theme.shadows.sm })}>
-        <Jumbotron media={media.data} corpusStats={corpusStats.data} />
-        <Waves height={40} width={150} />
-        <Media media={media.data} />
-        <Waves height={42} width={150} alt />
-        <Community />
-      </Box>
-      <Footer />
-    </>
-  );
-}
 
 export default Home;
 
-export const getServerSideProps = async () => {
-  const queryClient = new QueryClient();
-  await Promise.all([usePrefetchMedia(queryClient), usePrefetchCorpusStats(queryClient)]);
+export const getServerSideProps: GetServerSideProps = async () => {
+  const mediaUrl = API.V1.corpus.media.external;
+  const corpusStatsUrl = `${API.V1.corpus.stats.corpus.external}?type=word`;
+
+  const [mediaResult, corpusStatsResult] = await Promise.all([
+    fetchMedia(mediaUrl),
+    fetchCorpusStats(corpusStatsUrl),
+  ]);
 
   return {
     props: {
-      dehydratedState: dehydrate(queryClient),
+      fallback: {
+        [mediaUrl]: mediaResult?.data,
+        [corpusStatsUrl]: corpusStatsResult?.data,
+      },
     },
   };
 };
