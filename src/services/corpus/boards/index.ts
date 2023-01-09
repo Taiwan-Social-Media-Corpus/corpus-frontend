d42e388aa9dc9af62a3da91d5d6c638dd81c3b2c
Retr0327 <philcoke35@gmail.com>
2023-01-09 19:51:12 +0800

    Refactor to adapt to swr

--- src/services/corpus/boards/index.ts
+++ src/services/corpus/boards/index.ts
@@ -1,31 +1,17 @@
+import useSWR from 'swr';
 import API from '@config/api';
 import { Response } from 'types';
 import request from '@utils/request';
 import { Boards } from 'types/corpus';
-import { QueryClient, useQuery } from '@tanstack/react-query';
 
 type ResponseType = Response<Boards> | null;
 
-const QUERY_KEY = 'boards';
-
-const fetchBoards = (apiType: 'root' | 'external') => async () => {
-  const url = API.V1.corpus.boards[apiType];
-  return request({ url, method: 'GET' });
-};
+const fetchBoards = (url: string): Promise<ResponseType> => request({ url, method: 'GET' });
 
 const useBoards = () => {
-  const { data, isLoading, isError } = useQuery<ResponseType, any>({
-    queryKey: [QUERY_KEY],
-    queryFn: fetchBoards('root'),
-  });
+  const { data, error } = useSWR<Boards>(API.V1.corpus.boards.external);
 
-  return { boards: data, isLoading, isError };
+  return { boards: data, isLoading: !error && !data, error };
 };
 
-const usePrefetchBoards = async (client: QueryClient) =>
-  client.prefetchQuery({
-    queryKey: [QUERY_KEY],
-    queryFn: fetchBoards('external'),
-  });
-
-export { useBoards, usePrefetchBoards };
+export { useBoards, fetchBoards };
