7e2693dcf1b84b32626941c1c81163d3c052c91a
Retr0327 <philcoke35@gmail.com>
2023-01-10 10:59:40 +0800

    Update corpus stats api url

--- src/services/corpus/stats/corpus/index.ts
+++ src/services/corpus/stats/corpus/index.ts
@@ -9,7 +9,7 @@ type ResponseType = Response<CorpusStats> | null;
 const fetchCorpusStats = (url: string): Promise<ResponseType> => request({ url, method: 'GET' });
 
 const useCorpusStats = () => {
-  const url = `${API.V1.corpus.stats.corpus.external}?type=word`;
+  const url = API.V1.corpus.stats.corpus;
   const { data, error } = useSWR<CorpusStats>(url);
 
   return { corpusStats: data, isCorpusStatsLoading: !error && !data, corpusStatsError: error };
