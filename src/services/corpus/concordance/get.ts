9a615e84449a95a450d0d5e17cb667acb4047234
Retr0327 <philcoke35@gmail.com>
2023-01-10 20:01:54 +0800

    Refactor to adapt to swr

--- src/services/corpus/concordance/get.ts
+++ src/services/corpus/concordance/get.ts
@@ -1,41 +1,17 @@
+import useSWR from 'swr';
 import API from '@config/api';
 import { Response } from 'types';
 import request from '@utils/request';
 import { Concordance, ConcordanceRequestBody } from 'types/corpus';
-import { QueryClient, useQuery, UseQueryOptions } from '@tanstack/react-query';
 
-type ResponseType = Response<Concordance>;
-
-function buildOptions(payload: ConcordanceRequestBody, prefetch: boolean = false) {
-  const page = prefetch ? payload.page + 1 : payload.page;
-
-  const options: UseQueryOptions<ResponseType> = {
-    queryKey: ['concordance', page],
-    queryFn: () =>
-      request({
-        url: API.V1.corpus.concordance.root,
-        method: 'POST',
-        payload: { ...payload, page },
-      }),
-  };
-
-  if (!prefetch) {
-    options.keepPreviousData = true;
-    options.staleTime = 5000;
-  }
-
-  return options;
-}
+export type ResponseType = Response<Concordance> | null;
 
 const useConcordance = (payload: ConcordanceRequestBody) => {
-  const options = buildOptions(payload);
-  const { data, isError, isLoading, isPreviousData } = useQuery<ResponseType, any>(options);
-  return { concordance: data, isError, isLoading, isPreviousData };
+  const { data, error, mutate } = useSWR<ResponseType>(API.V1.corpus.concordance.root, (url) =>
+    request({ url, method: 'POST', payload })
+  );
+
+  return { concordance: data, isLoading: !error && !data, isError: error, mutate };
 };
 
-const usePrefetchConcordance = (client: QueryClient, payload: ConcordanceRequestBody) => {
-  const options = buildOptions(payload, true);
-  return client.prefetchQuery(options);
-};
-
-export { useConcordance, usePrefetchConcordance };
+export default useConcordance;
