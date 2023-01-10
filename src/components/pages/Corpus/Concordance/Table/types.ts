25c5740ed591448d749acfa67de9212e6e2ebe05
Retr0327 <philcoke35@gmail.com>
2023-01-10 20:08:38 +0800

    Update TableProps

--- src/components/pages/Corpus/Concordance/Table/types.ts
+++ src/components/pages/Corpus/Concordance/Table/types.ts
@@ -1,11 +1,6 @@
 import { Concordance } from 'types/corpus';
-import { Dispatch, SetStateAction } from 'react';
-import { PaginationState } from '@tanstack/react-table';
 
 export type TableProps = {
   data: Concordance;
   showPos: boolean;
-  pagination: PaginationState;
-  setPagination: Dispatch<SetStateAction<PaginationState>>;
-  fetchNumber: number;
 };
