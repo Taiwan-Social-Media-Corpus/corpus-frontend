a0f1228ab7dca1b3d3ec1e2de214af2022b54293
Retr0327 <philcoke35@gmail.com>
2023-01-12 19:22:40 +0800

    Implement user activation service

---
+++ src/services/user/activation/activate/index.ts
@@ -0,0 +1,24 @@
+import API from '@config/api';
+import { Response } from 'types';
+import { User } from 'types/user';
+import request from '@utils/request';
+
+type PayloadType = { code: string };
+
+type ResData = Pick<User, 'uid' | 'enabled'> & {
+  errorCount: number;
+  resendCount: number;
+};
+
+export type ResponseType = Response<ResData> | null;
+
+const activateUser = async (payload: PayloadType): Promise<[ResponseType, any]> => {
+  try {
+    const result = await request({ method: 'POST', url: API.V1.user.activation.root, payload });
+    return [result, null];
+  } catch (error: any) {
+    return [null, error];
+  }
+};
+
+export default activateUser;
