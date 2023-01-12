dc69c8aed6a77d21a92a368add053ffc7ad70ca5
Retr0327 <philcoke35@gmail.com>
2023-01-12 20:17:08 +0800

    Remove necessary utils

--- src/utils/layout/index.ts
+++
@@ -1,8 +0,0 @@
-import Route from '@config/routes';
-
-function shouldIncludeNavbar(path: string) {
-  const includeNavbarPaths = [Route.about.root, Route.guide];
-  return includeNavbarPaths.some((route) => path.startsWith(route));
-}
-
-export default shouldIncludeNavbar;
