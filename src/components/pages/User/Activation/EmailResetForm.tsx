4268baddfc85cafd05a179ffb6ea19280968ae09
Retr0327 <philcoke35@gmail.com>
2023-01-12 19:43:53 +0800

    Add EmailResetForm

---
+++ src/components/pages/User/Activation/EmailResetForm.tsx
@@ -0,0 +1,98 @@
+import { z } from 'zod';
+import useForm from '@hooks/Form';
+import { User } from 'types/user';
+import { memo, useState } from 'react';
+import { useRouter } from 'next/router';
+import { IconCircleCheck } from '@tabler/icons';
+import resetEmail from '@services/user/activation/email';
+import { Button, Text, Group, Stack, useMantineTheme } from '@mantine/core';
+
+type FieldValues = Pick<User, 'email'>;
+type Props = { handleClose: () => void };
+
+function EmailResetForm(props: Props) {
+  const { handleClose } = props;
+  const router = useRouter();
+  const theme = useMantineTheme();
+  const color = theme.colors.blue[9];
+  const [success, setSuccess] = useState(false);
+
+  const [Form, methods] = useForm<FieldValues>({
+    defaultValues: { email: '' },
+    onSubmit: async (data) => {
+      const { email } = data;
+      const [result, error] = await resetEmail({ email });
+      const { setError } = methods;
+
+      if (result == null || error) {
+        return router.push('/500', { pathname: router.pathname });
+      }
+
+      if (result.status === 'failed') {
+        switch (result.msg) {
+          case 'identical':
+            return setError('email', { message: '請輸入新的 email' });
+
+          case 'email unavailable':
+            return setError('email', { message: '電子郵件無法使用或已經有人使用。' });
+
+          default:
+            return setError('email', { message: '超出寄信限制' });
+        }
+      }
+
+      setSuccess(true);
+
+      const timeout = setTimeout(() => handleClose(), 1000);
+      clearTimeout(timeout);
+
+      return null;
+    },
+    schema: z.object({
+      email: z.string().min(1, '必填').email('請確認 email 格式'),
+    }),
+    controllers: {
+      email: {
+        label: '電子信箱',
+        name: 'email',
+        control: 'text-input',
+        withAsterisk: true,
+      },
+    },
+  });
+
+  return success ? (
+    <Stack align="center" spacing={10}>
+      <IconCircleCheck size={80} stroke={1.5} color={theme.colors.teal[7]} />
+      <Text color={color} weight="bold" size="xl">
+        您已成功更改電子信箱
+      </Text>
+      <Text color={color} weight="bold" size="xl">
+        請至信箱收信:
+      </Text>
+      <Text color={color} weight="bold" size="xl">
+        {methods.getValues('email')}
+      </Text>
+    </Stack>
+  ) : (
+    <Form>
+      <Group mt={30} sx={{ justifyContent: 'flex-end' }}>
+        <Button
+          sx={{
+            '&:hover': {
+              background: '#ccd0d5',
+            },
+            backgroundColor: '#ccc',
+            color: 'black',
+          }}
+          onClick={handleClose}
+        >
+          取消
+        </Button>
+        <Form.Button loading={methods.formState.isSubmitting}>確認</Form.Button>
+      </Group>
+    </Form>
+  );
+}
+
+export default memo(EmailResetForm);
