860a3b84e40262d74d1bdbe962a4394d5fc2df48
Retr0327 <philcoke35@gmail.com>
2023-01-15 17:42:00 +0800

    Add Header styles

---
+++ src/components/layout/Navigation/Header/Header.styles.ts
@@ -0,0 +1,54 @@
+import meta from 'meta';
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    paddingLeft: theme.spacing.md,
+    paddingRight: 'var(--removed-scroll-width, 0px)',
+    borderBottom: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.gray[8] : theme.colors.gray[3]
+    }`,
+    top: 0,
+    left: 0,
+    right: 0,
+    height: meta.layout.header.height,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    position: 'fixed',
+  },
+
+  logo: {
+    paddingRight: 35,
+    paddingLeft: theme.spacing.md,
+    height: meta.layout.header.height,
+    paddingTop: 6,
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  logoWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    pointerEvents: 'all',
+  },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  version: {
+    ...theme.fn.focusStyles(),
+    fontWeight: 700,
+    textDecoration: 'none',
+    marginTop: 5,
+
+    [theme.fn.smallerThan(860)]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default useStyles;
