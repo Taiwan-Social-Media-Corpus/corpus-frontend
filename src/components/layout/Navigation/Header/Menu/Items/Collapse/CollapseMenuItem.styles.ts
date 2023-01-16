902003fb0ed35b0bb84167ec7e87c5d5bc9ae2a3
Retr0327 <philcoke35@gmail.com>
2023-01-16 22:58:11 +0800

    Add CollapseMenuItem styles

---
+++ src/components/layout/Navigation/Header/Menu/Items/Collapse/CollapseMenuItem.styles.ts
@@ -0,0 +1,36 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  control: {
+    display: 'block',
+    width: '100%',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    fontSize: theme.fontSizes.sm,
+    paddingLeft: 2,
+  },
+
+  link: {
+    fontWeight: 500,
+    display: 'block',
+    textDecoration: 'none',
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    paddingLeft: 20,
+    marginLeft: 10,
+    fontSize: theme.fontSizes.xs,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    borderLeft: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    },
+  },
+
+  chevron: {
+    transition: 'transform 200ms ease',
+  },
+}));
+
+export default useStyles;
