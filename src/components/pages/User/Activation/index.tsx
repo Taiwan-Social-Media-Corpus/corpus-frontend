c2443fdb556ae8b06acc4048acc892ac22596c5e
Retr0327 <philcoke35@gmail.com>
2023-01-18 00:42:12 +0800

    Implement Modal by using `openModal`

--- src/components/pages/User/Activation/index.tsx
+++ src/components/pages/User/Activation/index.tsx
@@ -1,11 +1,12 @@
+import { memo } from 'react';
 import Route from '@config/routes';
-import { memo, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useUser } from '@contexts/User';
+import { Anchor, Title } from '@mantine/core';
 import AlertAction from '@contexts/Alert/actions';
-import { Anchor, Modal, Title } from '@mantine/core';
 import PinContainer from '@components/common/ui/Pin';
 import { getRedirectURL } from '@utils/url/redirect';
+import { openModal, closeAllModals } from '@mantine/modals';
 import activateUser from '@services/user/activation/activate';
 import { combineToString } from '@components/common/ui/Form/components/utils/pin';
 import EmailResetForm from './EmailResetForm';
@@ -14,25 +15,26 @@ import { handleResendCode, handleErrorCode } from './utils';
 function ActivationForm() {
   const router = useRouter();
   const { mutate } = useUser();
-  const [opened, setOpened] = useState(false);
 
   return (
     <PinContainer
       helper={(alert, dispatch) => (
         <>
-          <Modal
-            title={<Title order={2}>更改 Email 地址</Title>}
-            onClose={() => setOpened(false)}
-            opened={opened}
-            zIndex={999999}
-          >
-            <EmailResetForm handleClose={() => setOpened(false)} />
-          </Modal>
           <Anchor<'button'> weight={700} onClick={() => handleResendCode(dispatch, router)}>
             重新傳送通知
           </Anchor>{' '}
           或{' '}
-          <Anchor<'button'> weight={700} onClick={() => setOpened(true)}>
+          <Anchor<'button'>
+            weight={700}
+            onClick={() =>
+              openModal({
+                title: <Title order={2}>更改 Email 地址</Title>,
+                zIndex: 9999,
+                overlayOpacity: 0.55,
+                children: <EmailResetForm handleClose={closeAllModals} />,
+              })
+            }
+          >
             更改 Email 地址
           </Anchor>
         </>
