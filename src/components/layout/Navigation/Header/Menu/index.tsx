4dc44a8fc309a878ab47ae8d53e4223fbb57ca88
Retr0327 <philcoke35@gmail.com>
2023-01-17 12:16:25 +0800

    Add Menu

---
+++ src/components/layout/Navigation/Header/Menu/index.tsx
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import dynamic from 'next/dynamic';
+import { useDisclosure } from '@mantine/hooks';
+import { memo, useState, useMemo } from 'react';
+import { Burger, Menu as MantineMenu } from '@mantine/core';
+import { MenuProps } from './types';
+import createMenuLinks from './links';
+import useStyles from './Menu.styles';
+
+const UserButton = dynamic(() => import('./UserButton'));
+const CollapseMenuItem = dynamic(() => import('./Collapse'));
+
+function Menu(props: MenuProps) {
+  const { hasSession, firstName, lastName, fullName, email } = props;
+  const { classes } = useStyles();
+  const { toggle } = useDisclosure(false)[1];
+  const [menuOpened, setMenuOpened] = useState(false);
+  const links = createMenuLinks(hasSession);
+
+  const items = useMemo(
+    () =>
+      links.map((link, index) => {
+        if (link.links !== undefined) {
+          return (
+            <CollapseMenuItem
+              key={`${link.label}-${index}`}
+              label={link.label}
+              icon={<link.icon size={14} stroke={1.5} />}
+              links={link.links}
+            />
+          );
+        }
+
+        return (
+          <MantineMenu.Item
+            key={`${link.label}-${index}`}
+            component={Link}
+            href={link.link!}
+            icon={<link.icon size={14} stroke={1.5} />}
+          >
+            {link.label}
+          </MantineMenu.Item>
+        );
+      }),
+    [links]
+  );
+
+  const userButton = useMemo(
+    () =>
+      hasSession ? (
+        <MantineMenu.Item>
+          <UserButton firstName={firstName} lastName={lastName} fullName={fullName} email={email} />
+        </MantineMenu.Item>
+      ) : null,
+    [hasSession]
+  );
+
+  return (
+    <MantineMenu
+      shadow="md"
+      position="bottom"
+      width="100%"
+      offset={15}
+      onOpen={() => setMenuOpened(true)}
+      onClose={() => setMenuOpened(false)}
+      classNames={classes}
+    >
+      <MantineMenu.Target>
+        <Burger opened={menuOpened} onClick={toggle} className={classes.burger} size="sm" />
+      </MantineMenu.Target>
+
+      <MantineMenu.Dropdown>
+        {userButton}
+        {items}
+      </MantineMenu.Dropdown>
+    </MantineMenu>
+  );
+}
+
+export default memo(Menu);
