fccb09303c91e174ace7f34ee08860b99bd21d7e
Retr0327 <philcoke35@gmail.com>
2023-01-17 22:41:36 +0800

    Refactor request

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,37 +1,38 @@
 import { Request } from 'types';
 
-async function request<T>({ url, method, payload, authToken, toJson = true }: Request<T>) {
+async function request<T>({
+  url,
+  method,
+  payload,
+  headers,
+  toJson = true,
+  includeApiKey = false,
+}: Request<T>) {
   const _payload = payload ?? {};
   const body = method !== 'GET' ? JSON.stringify(_payload) : null;
-  const headers = new Headers({
+
+  const _headers = new Headers({
     Accept: 'application/json',
     'Content-Type': 'application/json',
+    ...headers,
   });
 
-  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+  if (includeApiKey) {
+    const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-  if (!API_KEY) {
-    throw new Error('API_KEY undefined');
+    if (!API_KEY) {
+      throw new Error('API_KEY undefined');
+    }
+
+    _headers.append('X-API-KEY', API_KEY);
   }
-  headers.append('X-API-KEY', API_KEY);
-
-  if (authToken) {
-    headers.append('Authorization', authToken);
-  }
-
-  // Request Timeout
-  const controller = new AbortController();
-  const id = setTimeout(() => controller.abort(), 5 * 1000);
 
   const response = await fetch(url, {
     credentials: 'include',
     method,
     body,
-    headers,
-    signal: controller.signal,
+    headers: _headers,
   });
-
-  clearTimeout(id);
 
   if (!toJson) {
     return response;
