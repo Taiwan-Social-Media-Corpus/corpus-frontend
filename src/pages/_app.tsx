1a1941f064e93eeb1e6a534c277c54158287d56e
Retr0327 <philcoke35@gmail.com>
2023-01-17 14:51:50 +0800

    Update according to official Mantine template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,34 @@
-import Head from 'next/head';
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
+import Layout from '@components/layout';
 import { getCookie } from 'cookies-next';
 import { AppPropsWithLayout } from 'types';
 import { ColorScheme } from '@mantine/core';
 import { UserProvider } from '@contexts/User';
-import { GetServerSidePropsContext } from 'next';
+import NextApp, { AppContext } from 'next/app';
 import withSwitch from '@components/common/auth';
 
 const DarkThemeContext = dynamic(() => import('@contexts/DarkTheme'));
-const NavigationLayout = dynamic(() => import('@components/layout/Navigation'));
 
 function App(props: AppPropsWithLayout & { colorScheme: ColorScheme }) {
   const { Component, pageProps, colorScheme } = props;
-  const router = useRouter();
   const getLayout = Component.getLayout ?? ((page) => page);
   const VerifiedComponent = withSwitch(Component);
 
   return (
-    <>
-      <Head>
-        <title>Taiwan Social Media Corpus</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link
-          rel="shortcut icon"
-          href={`${router.basePath}/favicon.ico`}
-          type="image/png"
-          sizes="32x32"
-        />
-        <link
-          rel="shortcut icon"
-          href={`${router.basePath}/favicon.svg`}
-          type="image/svg+xml"
-          sizes="any"
-        />
-      </Head>
-
-      <UserProvider>
-        <DarkThemeContext colorScheme={colorScheme}>
-          <NavigationLayout>{getLayout(<VerifiedComponent {...pageProps} />)}</NavigationLayout>
-        </DarkThemeContext>
-      </UserProvider>
-    </>
+    <UserProvider>
+      <DarkThemeContext colorScheme={colorScheme}>
+        <Layout>{getLayout(<VerifiedComponent {...pageProps} />)}</Layout>
+      </DarkThemeContext>
+    </UserProvider>
   );
 }
 
-App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
-});
+App.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+  return {
+    ...appProps,
+    colorScheme: getCookie('mantine-color-scheme', appContext.ctx) || 'light',
+  };
+};
 
 export default App;
