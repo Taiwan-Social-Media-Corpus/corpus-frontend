87e1536ad67ef100137e1b73f9b3db7d1a0e5948
Retr0327 <philcoke35@gmail.com>
2023-01-18 00:24:35 +0800

    Reframe AnchorDesktop

--- src/components/pages/User/Login/Callout/Anchor/Desktop/index.tsx
+++ src/components/pages/User/Login/Callout/Anchor/Desktop/index.tsx
@@ -1,35 +1,35 @@
-import { memo, useState } from 'react';
+import { memo } from 'react';
+import { openModal } from '@mantine/modals';
+import { Anchor, Paper, useMantineTheme } from '@mantine/core';
 import SignUpForm from '@components/pages/User/SignUp/Form/Desktop';
-import { Anchor, Modal, Paper, useMantineTheme } from '@mantine/core';
 
 function AnchorDesktop() {
   const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
 
   return (
-    <>
-      <Modal
-        opened={opened}
-        title="Sign up"
-        onClose={() => setOpened(false)}
-        overlayOpacity={0.55}
-        zIndex={9999}
-      >
-        <Paper
-          p={0}
-          style={{
-            position: 'relative',
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-          }}
-        >
-          <SignUpForm />
-        </Paper>
-      </Modal>
-
-      <Anchor onClick={() => setOpened(true)} size="md">
-        Create account
-      </Anchor>
-    </>
+    <Anchor
+      size="md"
+      onClick={() => {
+        openModal({
+          title: 'Sign up',
+          zIndex: 99999,
+          overlayOpacity: 0.55,
+          children: (
+            <Paper
+              p={0}
+              style={{
+                position: 'relative',
+                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+              }}
+            >
+              <SignUpForm isModal />
+            </Paper>
+          ),
+        });
+      }}
+    >
+      Create account
+    </Anchor>
   );
 }
 
