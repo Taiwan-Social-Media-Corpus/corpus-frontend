f8921227c5bbe54cf5a235597a18bc0e35c29fa6
Retr0327 <philcoke35@gmail.com>
2023-01-19 00:43:21 +0800

    Add RequestForm

---
+++ src/components/pages/User/Dashboard/Token/RequestButton/Form.tsx
@@ -0,0 +1,98 @@
+import { z } from 'zod';
+import useForm from '@hooks/Form';
+import Route from '@config/routes';
+import { useRouter } from 'next/router';
+import { IconCircleCheck } from '@tabler/icons';
+import { closeAllModals } from '@mantine/modals';
+import { memo, useState, useEffect } from 'react';
+import createAPIToken from '@services/user/apiToken/create';
+import { useAPITokens } from '@services/user/apiToken/read';
+import { Stack, Text, useMantineTheme } from '@mantine/core';
+
+type FieldValues = { confirmText: string };
+
+function RequestForm() {
+  const router = useRouter();
+  const { mutate } = useAPITokens();
+  const [disabled, setDisabled] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const theme = useMantineTheme();
+  const color = theme.colors.blue[9];
+
+  const [Form, methods] = useForm<FieldValues>({
+    defaultValues: { confirmText: '' },
+    schema: z.object({
+      confirmText: z.string(),
+    }),
+    controllers: {
+      confirmText: {
+        control: 'text-input',
+        name: 'confirmText',
+        label: (
+          <Text span sx={{ cursor: 'text' }} size="md">
+            Please type{' '}
+            <Text component="span" fw={700} sx={{ cursor: 'text' }} size="md">
+              Corpus/api-token
+            </Text>{' '}
+            to confirm.
+          </Text>
+        ),
+        placeholder: 'Corpus/api-token',
+        autoFocus: true,
+      },
+    },
+    onSubmit: async () => {
+      const [result, error] = await createAPIToken();
+
+      if (result === null || error) {
+        return router.push(Route.serverError, { pathname: router.pathname });
+      }
+
+      if (result.msg === 'exceeded apiToken rate') {
+        methods.setError('confirmText', { message: '超過申請上限，請於 24 小時後再試。' });
+        return setTimeout(() => {
+          closeAllModals();
+        }, 1000);
+      }
+
+      mutate();
+      setSuccess(true);
+      return setTimeout(() => {
+        router.replace(router.asPath);
+        closeAllModals();
+      }, 1000);
+    },
+  });
+
+  useEffect(() => {
+    if (methods.watch('confirmText') === 'Corpus/api-token') {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [methods.watch('confirmText')]);
+
+  const hasInputError = methods.formState.errors.confirmText !== undefined;
+
+  return success ? (
+    <Stack align="center" spacing={10}>
+      <IconCircleCheck size={80} stroke={1.5} color={theme.colors.teal[7]} />
+      <Text color={color} weight="bold" size="xl">
+        已成功建立！
+      </Text>
+    </Stack>
+  ) : (
+    <Form>
+      <Form.Button
+        loading={methods.formState.isSubmitting}
+        fullWidth
+        mt={hasInputError ? 35 : 'md'}
+        disabled={disabled}
+      >
+        確認
+      </Form.Button>
+    </Form>
+  );
+}
+
+export default memo(RequestForm);
