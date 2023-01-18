a2b60c5831c7bd8fd683db3fdabf6f1a46d350ed
Retr0327 <philcoke35@gmail.com>
2023-01-18 19:09:07 +0800

    Add APITokenTable

---
+++ src/components/pages/User/Dashboard/Token/Table/index.tsx
@@ -0,0 +1,27 @@
+import { memo } from 'react';
+import { APIToken } from 'types/user';
+import Table from '@components/common/ui/Table';
+import buildColumns from './Columns';
+
+type Props = { data: APIToken[] };
+
+function APITokenTable(props: Props) {
+  const { data } = props;
+  const columns = buildColumns();
+
+  return (
+    <Table
+      mt={50}
+      data={data}
+      columns={columns}
+      withColumnBorders
+      withBorder
+      fontSize="md"
+      horizontalSpacing="md"
+      sx={{ tableLayout: 'fixed', minWidth: 700, cursor: 'pointer' }}
+      style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}
+    />
+  );
+}
+
+export default memo(APITokenTable);
