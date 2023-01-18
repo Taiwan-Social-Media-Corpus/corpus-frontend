54512c43841e5e920199a433efaa5398b4496768
Retr0327 <philcoke35@gmail.com>
2023-01-18 19:14:57 +0800

    Implement table columns builder function

---
+++ src/components/pages/User/Dashboard/Token/Table/Columns/index.tsx
@@ -0,0 +1,41 @@
+import { format } from 'date-fns';
+import { APIToken } from 'types/user';
+import { zhTW } from 'date-fns/locale';
+import { ColumnDef } from '@tanstack/react-table';
+import ColumnHeader from '@components/common/ui/Table/components/Header';
+import HelperButtons from './Buttons';
+
+function formatTime(date: Date) {
+  return format(new Date(date), 'yyyy/MM/dd HH:mm', { locale: zhTW });
+}
+
+function buildColumns(): ColumnDef<APIToken, any>[] {
+  return [
+    {
+      accessorKey: 'apiToken',
+      header: () => <ColumnHeader position="left" content="API Token" />,
+      cell: (info) => info.row.original.apiToken,
+      size: 100,
+    },
+    {
+      accessorKey: 'createdAt',
+      header: () => <ColumnHeader position="left" content="Creation Time" />,
+      cell: (info) => formatTime(info.row.original.createdAt),
+      size: 60,
+    },
+    {
+      accessorKey: 'expiredAt',
+      header: () => <ColumnHeader position="left" content="Expire" />,
+      cell: (info) => formatTime(info.row.original.expiredAt),
+      size: 60,
+    },
+    {
+      accessorKey: 'buttons',
+      header: '',
+      cell: (info) => <HelperButtons apiToken={info.row.original.apiToken} />,
+      size: 60,
+    },
+  ];
+}
+
+export default buildColumns;
