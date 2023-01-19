2a795a1a044567b4c0dd223d9416b5a4435e348b
Retr0327 <philcoke35@gmail.com>
2023-01-19 14:26:37 +0800

    Add columns

--- src/components/pages/User/Dashboard/Token/Table/Columns/index.tsx
+++ src/components/pages/User/Dashboard/Token/Table/Columns/index.tsx
@@ -9,33 +9,31 @@ function formatTime(date: Date) {
   return format(new Date(date), 'yyyy/MM/dd HH:mm', { locale: zhTW });
 }
 
-function buildColumns(): ColumnDef<APIToken, any>[] {
-  return [
-    {
-      accessorKey: 'apiToken',
-      header: () => <ColumnHeader position="left" content="API Token" />,
-      cell: (info) => <div style={{ cursor: 'text' }}>{info.row.original.apiToken}</div>,
-      size: 100,
-    },
-    {
-      accessorKey: 'createdAt',
-      header: () => <ColumnHeader position="left" content="Creation Time" />,
-      cell: (info) => formatTime(info.row.original.createdAt),
-      size: 60,
-    },
-    {
-      accessorKey: 'expiredAt',
-      header: () => <ColumnHeader position="left" content="Expire" />,
-      cell: (info) => formatTime(info.row.original.expiredAt),
-      size: 60,
-    },
-    {
-      accessorKey: 'buttons',
-      header: '',
-      cell: (info) => <HelperButtons apiToken={info.row.original.apiToken} />,
-      size: 60,
-    },
-  ];
-}
+const columns: ColumnDef<APIToken, any>[] = [
+  {
+    accessorKey: 'apiToken',
+    header: () => <ColumnHeader position="left" content="API Token" />,
+    cell: (info) => <div style={{ cursor: 'text' }}>{info.row.original.apiToken}</div>,
+    size: 100,
+  },
+  {
+    accessorKey: 'createdAt',
+    header: () => <ColumnHeader position="left" content="Creation Time" />,
+    cell: (info) => formatTime(info.row.original.createdAt),
+    size: 60,
+  },
+  {
+    accessorKey: 'expiredAt',
+    header: () => <ColumnHeader position="left" content="Expire" />,
+    cell: (info) => formatTime(info.row.original.expiredAt),
+    size: 60,
+  },
+  {
+    accessorKey: 'buttons',
+    header: '',
+    cell: (info) => <HelperButtons apiToken={info.row.original.apiToken} />,
+    size: 60,
+  },
+];
 
-export default buildColumns;
+export default columns;
