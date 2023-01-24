6a35509140d650e1315f6f5a7a9d6b5b4736a42f
Retr0327 <philcoke35@gmail.com>
2023-01-24 18:07:22 +0800

    Add layoutConfig

---
+++ src/config/layout.ts
@@ -0,0 +1,32 @@
+const layoutConfig = {
+  facebook: {
+    link: 'https://www.facebook.com/groups/174362969268953',
+    color: '#1778F2',
+  },
+
+  github: {
+    lopen: 'https://github.com/lopentu',
+    organization: 'https://github.com/Taiwan-Social-Media-Corpus',
+    discussions: {
+      backend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-backend/discussions',
+      frontend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-frontend/discussions',
+    },
+    releases: {
+      backend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-backend/releases',
+      frontend: 'https://github.com/Taiwan-Social-Media-Corpus/corpus-frontend/releases',
+    },
+  },
+
+  mdx: {
+    breakpoint: 1080,
+    contentWidth: 820,
+    tableOfContentsWidth: 260,
+    tabHeight: 46,
+    tabHeightMobile: 38,
+  },
+  header: { height: 60 },
+  navbar: { width: 260, breakpoint: 760 },
+  footer: { height: 400, heightTablet: 460, heightMobile: 320 },
+} as const;
+
+export default layoutConfig;
