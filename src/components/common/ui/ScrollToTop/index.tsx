1995f4a07419c68da0dfacc783cf44e6d4166e79
Retr0327 <philcoke35@gmail.com>
2023-01-30 22:38:02 +0800

    Add ScrollToTop

---
+++ src/components/common/ui/ScrollToTop/index.tsx
@@ -0,0 +1,28 @@
+import { memo } from 'react';
+import { IconArrowUp } from '@tabler/icons';
+import { useWindowScroll } from '@mantine/hooks';
+import { Affix, ActionIcon, Transition } from '@mantine/core';
+
+function ScrollToTop() {
+  const [scroll, scrollTo] = useWindowScroll();
+
+  return (
+    <Affix position={{ bottom: 20, right: 20 }}>
+      <Transition transition="slide-up" mounted={scroll.y > 0}>
+        {(transitionStyles) => (
+          <ActionIcon
+            style={transitionStyles}
+            size={35}
+            color="blue"
+            variant="filled"
+            onClick={() => scrollTo({ y: 0 })}
+          >
+            <IconArrowUp size={16} />
+          </ActionIcon>
+        )}
+      </Transition>
+    </Affix>
+  );
+}
+
+export default memo(ScrollToTop);
