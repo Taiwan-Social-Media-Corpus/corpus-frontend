3b364ee54e99a3d98cf3130da2bdd69732b40f31
Retr0327 <philcoke35@gmail.com>
2023-02-01 12:50:22 +0800

    Add CollapseMenuItem

---
+++ src/components/layout/Navigation/Header/Menu/Collapse/index.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import { NestedLinks } from 'types/link';
+import { IconChevronRight } from '@tabler/icons';
+import { memo, useState, ReactNode, useMemo } from 'react';
+import { Box, Group, Collapse, UnstyledButton, Text, Menu } from '@mantine/core';
+import useStyles from './CollapseMenuItem.styles';
+
+type Props = {
+  label: string;
+  icon: ReactNode;
+  links: NestedLinks;
+};
+
+function CollapseMenuItem(props: Props) {
+  const { label, icon, links } = props;
+  const { classes } = useStyles();
+  const [opened, setOpened] = useState(false);
+
+  const collapseItems = useMemo(
+    () =>
+      links.map((value, index) => (
+        <Text className={classes.link} key={`${value.label}-${index}`}>
+          <Menu.Item component={Link} href={value.link}>
+            {value.label}
+          </Menu.Item>
+        </Text>
+      )),
+    [links]
+  );
+
+  return (
+    <Box
+      sx={(theme) => ({
+        padding: theme.spacing.xs,
+      })}
+    >
+      <UnstyledButton onClick={() => setOpened((o) => !o)} className={classes.control}>
+        <Group position="apart">
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {icon}
+            <Box ml={9}>{label}</Box>
+          </Box>
+          <IconChevronRight
+            className={classes.chevron}
+            size={14}
+            stroke={1.5}
+            style={{
+              transform: opened ? 'rotate(90deg)' : 'none',
+            }}
+          />
+        </Group>
+      </UnstyledButton>
+      <Collapse in={opened}>{collapseItems}</Collapse>
+    </Box>
+  );
+}
+
+export default memo(CollapseMenuItem);
