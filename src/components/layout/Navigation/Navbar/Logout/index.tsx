e0e0add854b49ad7e312cb6c88b96ca39f523bce
Retr0327 <philcoke35@gmail.com>
2023-02-02 11:52:54 +0800

    Add LogoutSection

---
+++ src/components/layout/Navigation/Navbar/Logout/index.tsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import Link from 'next/link';
+import Route from '@config/routes';
+import { Navbar } from '@mantine/core';
+import { IconLogout } from '@tabler/icons';
+import useStyles from './LogoutSection.styles';
+
+function LogoutSection() {
+  const { classes } = useStyles();
+
+  return (
+    <Navbar.Section className={classes.footer} p={20}>
+      <Link href={Route.logout} className={classes.logoutLink}>
+        <IconLogout className={classes.logoutIcon} stroke={1.5} />
+        <span>Logout</span>
+      </Link>
+    </Navbar.Section>
+  );
+}
+
+export default memo(LogoutSection);
