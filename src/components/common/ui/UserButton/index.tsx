195801ddaaa9d9e88f5d249c045b1384268c368c
Retr0327 <philcoke35@gmail.com>
2023-02-02 11:36:50 +0800

    Add UserButton

---
+++ src/components/common/ui/UserButton/index.tsx
@@ -0,0 +1,33 @@
+import { memo } from 'react';
+import { User } from 'types/user';
+import { IconChevronRight } from '@tabler/icons';
+import { UnstyledButton, UnstyledButtonProps, Group, Text } from '@mantine/core';
+import Avatar from '../Avatar';
+import useStyles from './UserButton.styles';
+
+type UserProps = Pick<User, 'lastName' | 'firstName' | 'email'> & { fullName: string };
+type Props = Partial<UserProps> & UnstyledButtonProps;
+
+function UserButton(props: Props) {
+  const { firstName, lastName, fullName, email, ...buttonProps } = props;
+  const { classes } = useStyles();
+
+  return (
+    <UnstyledButton className={classes.user} {...buttonProps}>
+      <Group>
+        <Avatar firstName={firstName} lastName={lastName} skeletonHeight={40} />
+        <div style={{ flex: 1 }}>
+          <Text size="sm" weight={500}>
+            {fullName}
+          </Text>
+          <Text color="dimmed" size="xs">
+            {email}
+          </Text>
+        </div>
+        <IconChevronRight size={14} stroke={1.5} />
+      </Group>
+    </UnstyledButton>
+  );
+}
+
+export default memo(UserButton);
