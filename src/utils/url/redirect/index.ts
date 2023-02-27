026d78ffabd9dabbf6f5fee16b05ecbc551edf08
Retr0327 <philcoke35@gmail.com>
2023-02-27 17:34:19 +0800

    Refactor `Redirect` as a component of router.push

--- src/utils/url/redirect/index.ts
+++ src/utils/url/redirect/index.ts
@@ -1,4 +1,22 @@
-import Redirect from './redirect';
-import getRedirectURL from './url';
+import { useEffect } from 'react';
+import { useRouter, Router } from 'next/router';
 
-export { Redirect, getRedirectURL };
+type RouterPush = Parameters<Router['push']>;
+
+type RedirectProps = {
+  url: RouterPush[0];
+  as?: RouterPush[1];
+  options?: RouterPush[2];
+};
+
+function Redirect(props: RedirectProps) {
+  const { url, as, options } = props;
+  const router = useRouter();
+  useEffect(() => {
+    router.push(url, as, options);
+  }, [router.pathname]);
+
+  return null;
+}
+
+export default Redirect;
--- src/utils/url/redirect/url.ts
+++
@@ -1,6 +0,0 @@
-function getRedirectURL(url: string, asPath: string) {
-  const urlParams = new URLSearchParams(asPath);
-  return urlParams.get(url);
-}
-
-export default getRedirectURL;
