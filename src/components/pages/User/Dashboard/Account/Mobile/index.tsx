9257cfa26d1baad0fb0b2f76a740ba426f084fdf
Retr0327 <philcoke35@gmail.com>
2023-02-28 16:46:19 +0800

    Add MobileAccountPage

---
+++ src/components/pages/User/Dashboard/Account/Mobile/index.tsx
@@ -0,0 +1,55 @@
+import Route from '@config/routes';
+import { memo, useMemo } from 'react';
+import { useRouter } from 'next/router';
+import { Tabs, Group } from '@mantine/core';
+import { AccountPageProps } from '../types';
+
+function MobileAccountPage(props: AccountPageProps) {
+  const { blocks } = props;
+  const router = useRouter();
+
+  const tabsList = useMemo(
+    () =>
+      blocks.map((block, index) => (
+        <Tabs.Tab
+          key={`${block.name}-${index}`}
+          value={block.query}
+          icon={<block.icon size={14} />}
+        >
+          {block.name}
+        </Tabs.Tab>
+      )),
+    [blocks]
+  );
+
+  const tabsPanel = useMemo(
+    () =>
+      blocks.map((block, index) => (
+        <Tabs.Panel key={`${block.name}-${index}`} value={block.query} pt="xs">
+          {block.content}
+        </Tabs.Panel>
+      )),
+    [blocks]
+  );
+
+  return (
+    <Group grow sx={{ width: '90%' }}>
+      <Tabs
+        value={router.query.tab as string}
+        onTabChange={(value) =>
+          router.push({ pathname: Route.dashboard.account, query: { tab: value } })
+        }
+        sx={{
+          '@media (min-width: 992px)': {
+            display: 'none',
+          },
+        }}
+      >
+        <Tabs.List grow>{tabsList}</Tabs.List>
+        {tabsPanel}
+      </Tabs>
+    </Group>
+  );
+}
+
+export default memo(MobileAccountPage);
