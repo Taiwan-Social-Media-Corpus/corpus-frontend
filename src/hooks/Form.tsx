65ef6114a6ba7ae62fea21a5f9716eaff1aefabe
Retr0327 <philcoke35@gmail.com>
2023-03-02 16:10:30 +0800

    Refactor flow control for contollers

--- src/hooks/Form.tsx
+++ src/hooks/Form.tsx
@@ -10,10 +10,18 @@ import {
 } from 'react-hook-form';
 import { useId } from '@mantine/hooks';
 import React, { ReactNode } from 'react';
-import { FormControllerProps } from 'types/form';
+import { FormControllerProps, Controllers } from 'types/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import FormController from '@components/common/ui/Form';
-import { Button, Grid, ButtonProps, useMantineTheme } from '@mantine/core';
+import {
+  Button,
+  Grid,
+  ButtonProps,
+  useMantineTheme,
+  Box,
+  BoxProps,
+  GridProps,
+} from '@mantine/core';
 
 type AsyncDefaultValues<TFieldValues> = (payload?: unknown) => Promise<TFieldValues>;
 
@@ -33,43 +41,68 @@ const useForm = <TFieldValues extends FieldValues = FieldValues, TContext = any>
 ) => {
   const id = useId();
   const theme = useMantineTheme();
-  const { controllers, schema, defaultValues, onSubmit, onSubmitError, ...rest } = props;
+
+  const {
+    controllers: rawControllers,
+    schema,
+    defaultValues,
+    onSubmit,
+    onSubmitError,
+    ...rest
+  } = props;
+
   const methods = useHookForm<TFieldValues, TContext>({
     resolver: schema ? zodResolver(schema) : undefined,
     defaultValues: defaultValues as UseFormProps<TFieldValues, TContext>['defaultValues'],
+    mode: 'onTouched',
+    shouldFocusError: false,
     ...rest,
   });
 
-  const Form = ({
-    children,
-  }: {
-    children?: ReactNode | ((ctx: UseFormReturn<TFieldValues, TContext>) => ReactNode);
-  }) => (
-    <FormProvider {...methods}>
-      <form id={id} onSubmit={methods.handleSubmit(onSubmit, onSubmitError)}>
-        <Grid justify="center" gutter="xl">
-          {Object.values(controllers).map((field, index) => {
-            const { col, after, ...controllerProps } = field;
-            return (
-              <Grid.Col key={`${field.name}-${index}`} {...col}>
-                <FormController {...controllerProps} />
-                {typeof after === 'function' ? after(methods) : after}
-              </Grid.Col>
-            );
-          })}
-        </Grid>
-        {typeof children === 'function' ? children(methods) : children}
-      </form>
-    </FormProvider>
-  );
+  /* eslint-disable @typescript-eslint/no-shadow */
+  const Form = (
+    props: {
+      children?: ReactNode | ((ctx: UseFormReturn<TFieldValues, TContext>) => ReactNode);
+      grid?: Omit<GridProps, 'children'>;
+    } & Omit<BoxProps, 'children'>
+  ) => {
+    const { children, grid, ...rest } = props;
+    let controllers: Controllers<TFieldValues, TContext>;
 
-  Form.Button = (buttonProps: ButtonProps) => (
-    <Button
-      type="submit"
-      form={id}
-      loaderProps={{ color: theme.colors.blue[5] }}
-      {...buttonProps}
-    />
+    if (rawControllers instanceof Function) {
+      controllers = rawControllers(methods);
+    } else {
+      controllers = rawControllers;
+    }
+
+    return (
+      <FormProvider {...methods}>
+        <Box<'form'>
+          component="form"
+          id={id}
+          onSubmit={methods.handleSubmit(onSubmit, onSubmitError)}
+          {...rest}
+        >
+          <Grid justify="center" gutter="lg" {...grid}>
+            {Object.values(controllers).map((field, index) => {
+              const { col, after, ...controllerProps } = field;
+              return (
+                <Grid.Col key={`${field.name}-${index}`} {...col}>
+                  <FormController {...controllerProps} />
+                  {typeof after === 'function' ? after(methods) : after}
+                </Grid.Col>
+              );
+            })}
+          </Grid>
+          {typeof children === 'function' ? children(methods) : children}
+        </Box>
+      </FormProvider>
+    );
+  };
+
+  /* eslint-disable @typescript-eslint/no-shadow */
+  Form.Button = (props: ButtonProps) => (
+    <Button type="submit" form={id} loaderProps={{ color: theme.colors.blue[5] }} {...props} />
   );
 
   return [Form, methods] as const;
