e7d355547879b97743828b55dc263fbb63156a91
Retr0327 <philcoke35@gmail.com>
2023-03-02 17:44:46 +0800

    Add mdx components

--- src/components/layout/Mdx/Components/index.tsx
+++ src/components/layout/Mdx/Components/index.tsx
@@ -1,18 +1,20 @@
 import Head from 'next/head';
 import NextImage from 'next/image';
-import { Prism } from '@mantine/prism';
+import { MDXComponents } from 'types/mdx';
 import { Text, Code } from '@mantine/core';
+import Pre from './Pre';
 import Image from './Image';
 import Table from './Table';
 import MdxTitle from './Title';
 
 const h = (order: 1 | 2 | 3 | 4 | 5 | 6) => (props: any) => <MdxTitle order={order} {...props} />;
 
-const components = {
+const components: MDXComponents = {
   Head,
   NextImage,
-  table: Table,
   img: Image,
+  table: Table,
+  pre: Pre,
   h1: h(1),
   h2: h(2),
   h3: h(3),
@@ -31,22 +33,13 @@ const components = {
   ),
   li: (props: any) => <li {...props} style={{ marginTop: 4 }} />,
   a: (props: any) => {
+    const isHTTP: string = props.href.startsWith('http') && !props.href.startsWith('/');
+    const newTabProps = isHTTP ? { target: '_blank', rel: 'noopener noreferrer' } : undefined;
     const style = { fontSize: 15 };
     return (
-      <Text style={style} component="a" variant="link" href={props.href}>
+      <Text style={style} component="a" variant="link" href={props.href} {...newTabProps}>
         {props.children}
       </Text>
-    );
-  },
-  pre: (props: any) => {
-    const matches = (props.children.props.className || '').match(/language-(?<lang>.*)/);
-    return (
-      <Prism
-        language={matches && matches.groups && matches.groups.lang ? matches.groups.lang : ''}
-        mb={20}
-      >
-        {props.children.props.children}
-      </Prism>
     );
   },
 };
