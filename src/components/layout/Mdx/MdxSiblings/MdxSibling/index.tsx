293b4625eb894c3290e39c10da83326886f0a0f5
Retr0327 <philcoke35@gmail.com>
2023-03-02 17:48:01 +0800

    Add MdxSibling

---
+++ src/components/layout/Mdx/MdxSiblings/MdxSibling/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { Text } from '@mantine/core';
+import { Frontmatter } from 'types/mdx';
+import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react';
+import useStyles from './MdxSibling.styles';
+
+type MdxSiblingProps = {
+  data: Frontmatter;
+  type: 'next' | 'prev';
+};
+
+function MdxSibling(props: MdxSiblingProps) {
+  const { data, type } = props;
+  const { classes } = useStyles();
+
+  return (
+    <Link href={data.slug} className={classes.control}>
+      {type === 'prev' && <IconArrowLeft size={22} stroke={1.5} />}
+      <div className={classes.body}>
+        <Text size="lg" align={type === 'next' ? 'left' : 'right'}>
+          {type === 'next' ? 'Up next' : 'Go back'}
+        </Text>
+        <Text color="dimmed" size="sm" align={type === 'next' ? 'left' : 'right'}>
+          {data.title}
+        </Text>
+      </div>
+      {type === 'next' && <IconArrowRight size={22} stroke={1.5} />}
+    </Link>
+  );
+}
+
+export default MdxSibling;
