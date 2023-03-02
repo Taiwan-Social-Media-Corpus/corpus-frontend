be70f0153ab3274ca6143bbad2c57c15b8d03fa5
Retr0327 <philcoke35@gmail.com>
2023-03-02 21:04:36 +0800

    Implement object equality check function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,6 +7,23 @@ function isEmptyObject(value: object | null) {
   return isObject(value) && Object.entries(value).length === 0;
 }
 
+function isObjectsEqual(obj1: { [key: string]: any }, obj2: { [key: string]: any }) {
+  const obj1Props = Object.getOwnPropertyNames(obj1);
+  const obj2Props = Object.getOwnPropertyNames(obj2);
+
+  if (obj1Props.length !== obj2Props.length) return false;
+
+  for (let i = 0; i < obj1Props.length; i += 1) {
+    const propName = obj1Props[i];
+
+    if (obj1[propName] !== obj2[propName]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function sanitizeIP(ip: string) {
   return ip.replace('::ffff:', '');
 }
@@ -15,4 +32,4 @@ function getRouteFromPathname(pathname: string) {
   return pathname.replace(/^\/([^/]*).*$/, '$1');
 }
 
-export { isObject, isEmptyObject, sanitizeIP, getRouteFromPathname };
+export { isObject, isEmptyObject, isObjectsEqual, sanitizeIP, getRouteFromPathname };
