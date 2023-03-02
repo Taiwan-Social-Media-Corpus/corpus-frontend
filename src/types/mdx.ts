b5463b851d5ffa896412d758cd1fd7e196bb1866
Retr0327 <philcoke35@gmail.com>
2023-03-02 17:45:03 +0800

    Add MDXComponents type

--- src/types/mdx.ts
+++ src/types/mdx.ts
@@ -23,3 +23,17 @@ export type MdxLayoutProps = {
 export type MdxFolder = 'About' | 'Guide';
 
 export type Fields = 'slug' | 'frontMatter' | 'content';
+
+type FunctionComponent<Props> = (props: Props) => JSX.Element | null;
+type ClassComponent<Props> = new (props: Props) => JSX.ElementClass;
+type Component<Props> = FunctionComponent<Props> | ClassComponent<Props>;
+
+interface NestedMDXComponents {
+  [key: string]: NestedMDXComponents | Component<any> | keyof JSX.IntrinsicElements;
+}
+
+export type MDXComponents = NestedMDXComponents & {
+  [Key in keyof JSX.IntrinsicElements]?:
+    | Component<JSX.IntrinsicElements[Key]>
+    | keyof JSX.IntrinsicElements;
+};
