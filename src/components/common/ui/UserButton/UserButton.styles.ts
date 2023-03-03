791af6d6bceb6e017acf50001eae3019ad21159e
Retr0327 <philcoke35@gmail.com>
2023-03-03 21:25:58 +0800

    Update UserButton styles

--- src/components/common/ui/UserButton/UserButton.styles.ts
+++ src/components/common/ui/UserButton/UserButton.styles.ts
@@ -10,6 +10,23 @@ const useStyles = createStyles((theme) => ({
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
     },
   },
+
+  link: {
+    display: 'block',
+    textDecoration: 'none',
+    marginLeft: 10,
+    fontSize: theme.fontSizes.xs,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    },
+  },
+
+  chevron: {
+    transition: 'transform 200ms ease',
+  },
 }));
 
 export default useStyles;
