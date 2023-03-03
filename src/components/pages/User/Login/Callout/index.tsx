f3ce40bfda3540abed8545b92bd02ed0d8030a18
Retr0327 <philcoke35@gmail.com>
2023-03-03 23:47:51 +0800

    Reduce bundle size using dynamic import

--- src/components/pages/User/Login/Callout/index.tsx
+++ src/components/pages/User/Login/Callout/index.tsx
@@ -1,12 +1,17 @@
-import { memo } from 'react';
+import dynamic from 'next/dynamic';
 import { Text } from '@mantine/core';
+import { memo, useMemo } from 'react';
 import { useMediaQuery } from '@mantine/hooks';
-import AnchorMobile from './Anchor/Mobile';
-import AnchorDesktop from './Anchor/Desktop';
+
+const AnchorMobile = dynamic(() => import('./Anchor/Mobile'));
+const AnchorDesktop = dynamic(() => import('./Anchor/Desktop'));
 
 function LoginCallout() {
   const mobileScreen = useMediaQuery('(max-width: 420px)');
-  const anchor = mobileScreen ? <AnchorMobile /> : <AnchorDesktop />;
+  const anchor = useMemo(
+    () => (mobileScreen ? <AnchorMobile /> : <AnchorDesktop />),
+    [mobileScreen]
+  );
 
   return (
     <Text size="md" align="center" mt={5}>
